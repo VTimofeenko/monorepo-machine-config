441b69789dd501c993996598798a6d5410f7dc82
Vladimir Timofeenko <id@vtimofeenko.com>
2023-02-06 10:45:06 -0800

    feat: enable network scanning

--- modules/hardware/scanner.nix
+++ modules/hardware/scanner.nix
@@ -3,5 +3,7 @@
 {
   hardware.sane.enable = true;
   hardware.sane.extraBackends = [ pkgs.hplipWithPlugin ];
+  services.avahi.enable = true;
+  services.avahi.nssmdns = true;
   users.users.spacecadet.extraGroups = [ "scanner" ];
 }
