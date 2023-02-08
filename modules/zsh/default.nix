7d86b6d3f8255b0e52ea9e380650577bdde00feb
Vladimir Timofeenko <id@vtimofeenko.com>
2023-02-08 12:07:07 -0800

    fix: [zsh] correct place for highlight style specification

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -115,8 +115,6 @@ in
           zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*'
           zstyle ':completion:*' list-colors ''${(s.:.)LS_COLORS}
           zstyle ':completion:*' menu select
-          # Make comments visible on default background
-          ZSH_HIGHLIGHT_STYLES[comment]='none'
 
           # Automatically escape urls when pasting
           autoload -Uz url-quote-magic
@@ -167,6 +165,8 @@ in
           else
             toString null
          }
+        # Make comments visible on default background
+        ZSH_HIGHLIGHT_STYLES[comment]='none'
       '';
     };
   };
