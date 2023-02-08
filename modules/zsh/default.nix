59c7eefdde43f6af5423cd060ff9007fa72c0e61
Vladimir Timofeenko <id@vtimofeenko.com>
2023-02-08 08:53:46 -0800

    wip: [zsh] move to literate style

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -15,26 +15,16 @@ in
     gpg_enable = mkEnableOption "enable gpg-agent";
     enableAnyNixShell = mkEnableOption "enable any-nix-shell";
   };
+
   config = {
     environment.systemPackages = with pkgs; [
       fzf
       killall
       bat
-      # bookmark plugin
-      (writeTextFile {
-        name = "bookmarks.zsh";
-        text = ''${builtins.readFile ./plugins/bookmarks.zsh}'';
-        destination = "/share/zsh/site-functions/bookmarks.zsh";
-      })
-      # My cursor plugin
-      (writeTextFile {
-        name = "cursor_mode.zsh";
-        text = ''${builtins.readFile ./plugins/cursor_mode.zsh}'';
-        destination = "/share/zsh/site-functions/cursor_mode.zsh";
-      })
       # the next line conditionally installs direnv if it is enabled
       # just having pkgs.direnv is not enough, it does not get added to the path
     ] ++ (if cfg.direnv_enable then [ pkgs.direnv ] else [ ]);
+
     programs.zsh = {
       enable = true;
       enableCompletion = true;
@@ -84,75 +74,77 @@ in
         # cd management
         "AUTO_CD" # automatically cd into directory
       ];
-      interactiveShellInit = ''
-        # Enable vim editing of command line
-        ${builtins.readFile ./plugins/01-vim-edit.zsh}
-        # Enable cd +1..9 to go back in dir stack
-        ${builtins.readFile ./plugins/02-cd.zsh}
-        # fzf bindings
-        source ${pkgs.fzf}/share/fzf/key-bindings.zsh
+      interactiveShellInit =
+        ''
+          # Enable vim editing of command line
+          ${builtins.readFile ./plugins/01-vim-edit.zsh}
+          # Enable cd +1..9 to go back in dir stack
+          ${builtins.readFile ./plugins/02-cd.zsh}
+          # fzf bindings
+          source ${pkgs.fzf}/share/fzf/key-bindings.zsh
 
-        # Word Navigation shortcuts
-        bindkey "^A" vi-beginning-of-line
-        bindkey "^E" vi-end-of-line
-        bindkey "^F" end-of-line
+          # Word Navigation shortcuts
+          bindkey "^A" vi-beginning-of-line
+          bindkey "^E" vi-end-of-line
+          bindkey "^F" end-of-line
 
-        # ctrl+arrow for word jupming
-        bindkey "^[[1;5C" forward-word
-        bindkey "^[[1;5D" backward-word
+          # ctrl+arrow for word jupming
+          bindkey "^[[1;5C" forward-word
+          bindkey "^[[1;5D" backward-word
 
-        # alt+f forward a word
-        bindkey "^[f" forward-word
+          # alt+f forward a word
+          bindkey "^[f" forward-word
 
-        # alt+b back a word
-        bindkey "^[b" backward-word
-        # working backspace
-        bindkey -v '^?' backward-delete-char
+          # alt+b back a word
+          bindkey "^[b" backward-word
+          # working backspace
+          bindkey -v '^?' backward-delete-char
 
-        # Use vim keys in tab complete menu
-        zmodload zsh/complist
-        bindkey -M menuselect 'h' vi-backward-char
-        bindkey -M menuselect 'k' vi-up-line-or-history
-        bindkey -M menuselect 'l' vi-forward-char
-        bindkey -M menuselect 'j' vi-down-line-or-history
-        bindkey -M menuselect '^ ' accept-line
+          # Use vim keys in tab complete menu
+          zmodload zsh/complist
+          bindkey -M menuselect 'h' vi-backward-char
+          bindkey -M menuselect 'k' vi-up-line-or-history
+          bindkey -M menuselect 'l' vi-forward-char
+          bindkey -M menuselect 'j' vi-down-line-or-history
+          bindkey -M menuselect '^ ' accept-line
 
-        # Add entry by "+" but do not exit menuselect
-        bindkey -M menuselect "+" accept-and-menu-complete
-        # Color the completions
-        autoload -Uz compinit
-        zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*'
-        zstyle ':completion:*' list-colors ''${(s.:.)LS_COLORS}
-        zstyle ':completion:*' menu select
+          # Add entry by "+" but do not exit menuselect
+          bindkey -M menuselect "+" accept-and-menu-complete
+          # Color the completions
+          autoload -Uz compinit
+          zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*'
+          zstyle ':completion:*' list-colors ''${(s.:.)LS_COLORS}
+          zstyle ':completion:*' menu select
 
-        # Automatically escape urls when pasting
-        autoload -Uz url-quote-magic
-        zle -N self-insert url-quote-magic
-        autoload -Uz bracketed-paste-magic
-        zle -N bracketed-paste bracketed-paste-magic
+          # Automatically escape urls when pasting
+          autoload -Uz url-quote-magic
+          zle -N self-insert url-quote-magic
+          autoload -Uz bracketed-paste-magic
+          zle -N bracketed-paste bracketed-paste-magic
 
-        # Custom plugins (see call to pkgs.writeTextFile in the zsh.nix)
-        # Bookmarks by "@@"
-        autoload -Uz bookmarks.zsh && bookmarks.zsh
-        # Cursor mode block <> beam
-        autoload -Uz cursor_mode.zsh && cursor_mode.zsh
+          # Custom plugins can be quickly loaded if fpath is extended:
+          fpath=(${./plugins} $fpath)
+          # Bookmarks by "@@"
+          autoload -Uz bookmarks.zsh && bookmarks.zsh
+          # Cursor mode block <> beam
+          autoload -Uz cursor_mode.zsh && cursor_mode.zsh
 
-        # To use openpgp cards
-        ${if cfg.gpg_enable
-        then
-          ''
-          if (( $EUID != 0 )); then
-            export GPG_TTY="$(tty)"
-            ${pkgs.gnupg}/bin/gpg-connect-agent /bye
-            export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
-          fi
-          ''
-        else
-          toString null
-        }
-        # alias that creates the directory and changes into it
-        mkcd(){ mkdir -p "$@" && cd "$@"; }
-      '';
+          # To use openpgp cards
+          ${if cfg.gpg_enable
+          then
+            ''
+            if (( $EUID != 0 )); then
+              export GPG_TTY="$(tty)"
+              ${pkgs.gnupg}/bin/gpg-connect-agent /bye
+              export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
+            fi
+            ''
+          else
+            toString null
+          }
+          # alias that creates the directory and changes into it
+          mkcd(){ mkdir -p "$@" && cd "$@"; }
+        '';
       promptInit = ''
         ${if cfg.starship_enable
         then
