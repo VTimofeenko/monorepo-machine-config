fa1e1fd52666de664e63d9faa2eeb60ec26bf859
Vladimir Timofeenko <id@vtimofeenko.com>
2023-04-16 19:15:13 -0700

    chore: add sway-launcher but keep zsh as the default shell
    I am still working on the config - rather than restart greetd altogether
    it's easier to relaunch the desktop environment.

--- hosts/neptunium/nvidia2070.nix
+++ hosts/neptunium/nvidia2070.nix
@@ -4,5 +4,16 @@
   services.xserver.videoDrivers = [ "nvidia" ];
 
   # nvidia-drm.modeset=1 is required for some wayland compositors, e.g. sway
-  hardware.nvidia.modesetting.enable = true;
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = true;
+  };
+
+  environment.variables = {
+    # NOTE: needed for mouse cursor to be visible
+    WLR_NO_HARDWARE_CURSORS = "1";
+    WLR_RENDERER = "vulkan";
+  };
+  # Apps on sway seem to be unusable without this:
+  hardware.nvidia.forceFullCompositionPipeline = true;
 }
--- modules/sway/system/greeter.nix
+++ modules/sway/system/greeter.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ pkgs, lib, config, ... }:
 {
 
   boot.kernelParams = [ "console=tty1" ];
@@ -7,11 +7,18 @@
     vt = 2;
 
     settings = {
-      default_session = {
-        # taken from https://github.com/apognu/tuigreet
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd ${pkgs.zsh}/bin/zsh";
-        user = "greeter";
-      };
+      default_session =
+        let
+          sway-launcher = pkgs.writeShellScript "sway-launcher" ''
+            exec systemd-cat --identifier=sway ${pkgs.sway}/bin/sway ${if (config.networking.hostName == "neptunium") then "--unsupported-gpu" else ""}
+          '';
+        in
+        {
+          # taken from https://github.com/apognu/tuigreet
+          # command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd ${sway-launcher}";
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd ${pkgs.zsh}/bin/zsh";
+          user = "greeter";
+        };
     };
   };
 }
