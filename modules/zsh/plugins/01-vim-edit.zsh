7924e95056679c49168700614af273b89c312548
Vladimir Timofeenko <id@vtimofeenko.com>
2023-04-30 17:02:59 -0700

    chore: set edit cmd keys to 'jk' so as not to conflict with E - end of word

--- modules/zsh/plugins/01-vim-edit.zsh
+++ modules/zsh/plugins/01-vim-edit.zsh
@@ -2,4 +2,4 @@
 bindkey -v
 
 autoload edit-command-line; zle -N edit-command-line
-bindkey -M vicmd E edit-command-line  # uppercase E to edit the current line
+bindkey -M vicmd jk edit-command-line  # jk chord to edit the current line
