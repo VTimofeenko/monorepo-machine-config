67104e21a56d1278be8c2e0ff491d303e56dd55c
Vladimir Timofeenko <id@vtimofeenko.com>
2023-05-14 19:25:09 -0700

    chore: add stub for eww

--- modules/hyprland/eww/default.nix
+++ modules/hyprland/eww/default.nix
@@ -1,27 +1,43 @@
 { pkgs, config, lib, ... }:
 let
+  eww-scripts = import ./scripts.nix pkgs;
   eww-yuck =
     ''
-        (defwindow example
-                 :monitor 0
-                 :geometry (geometry
-                                     :width "100%"
-                                     :height "1%"
-                                     :anchor "top center"
-                           )
-                 :exclusive true	
-                 :stacking "fg"
-                 (button :class "tray" :halign "end" "''${time}")
-        )
+      (defwindow bar
+              :monitor 0
+              :geometry (geometry
+                                  :width "100%"
+                                  :height "1%"
+                                  :anchor "top center"
+                          )
+              :exclusive true	
+              :stacking "fg"
+              (button :class "tray" :halign "end" "''${time}")
+              (workspaces :halign "start")
+      )
 
-        (defpoll time :interval "20s"
-      	`date +" %b %d %R"`)
+      (defpoll time :interval "20s"
+          `date +" %b %d %R"`)
+            
+      (deflisten workspaces :initial "[]" "bash ${eww-scripts.get-workspaces}")
+        (deflisten current_workspace :initial "1" "bash ${eww-scripts.get-active-workspace}")
+        (defwidget workspaces []
+          (eventbox :onscroll "bash ${eww-scripts.change-active-workspace} {} ''${current_workspace}" :class "workspaces-widget"
+            (box :space-evenly true
+              (label :text "''${workspaces}''${current_workspace}" :visible false)
+              (for workspace in workspaces
+                (eventbox :onclick "hyprctl dispatch workspace ''${workspace.id}"
+                  (box :class "workspace-entry ''${workspace.id == current_workspace ? "current" : ""} ''${workspace.windows > 0 ? "occupied" : "empty"}"
+                    (label :text "''${workspace.id}")
+                    )
+                  )
+                )
+              )
+            )
+          )
+
     '';
-
-
-  eww-scss =
-    ''
-    '';
+  inherit (import ./css.nix) eww-scss;
 in
 {
   # Generating the module dynamically => not using home manager module
