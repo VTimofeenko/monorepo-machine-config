4b7c976070c9a6d64904d138d098b12908cdfae8
Vladimir Timofeenko <id@vtimofeenko.com>
2023-05-19 09:02:53 -0700

    chore: remove whitespace

--- homeConfigurations/zsh/default.nix
+++ homeConfigurations/zsh/default.nix
@@ -80,13 +80,13 @@
                       OLD_SELF_INSERT=''${''${(s.:.)widgets[self-insert]}[2,3]}
                       zle -N self-insert url-quote-magic # I wonder if you'd need `.url-quote-magic`?
                     }
-          
+
                     pastefinish() {
                       zle -N self-insert $OLD_SELF_INSERT
                     }
                     zstyle :bracketed-paste-magic paste-init pasteinit
                     zstyle :bracketed-paste-magic paste-finish pastefinish
-          
+
         ''
         + # set SSH_AUTH_SOCK <=> gpg-agent is enabled in home-manager
         (if config.services.gpg-agent.enable
