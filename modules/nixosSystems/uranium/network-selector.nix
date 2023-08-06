f2b3782d88cc1106a734ac3f7e3f282c7fb600bd
Vladimir Timofeenko <id@vtimofeenko.com>
2023-08-05 17:23:10 -0700

    fix: add arguments to adhoc-wifi import

--- modules/nixosSystems/uranium/network-selector.nix
+++ modules/nixosSystems/uranium/network-selector.nix
@@ -6,7 +6,7 @@ in
 lib.mkMerge [
   (lib.mkIf (cfg != "wifi-lan") (import ../../network/public-firewall.nix { inherit lib; }))
   (lib.mkIf (cfg == "eth") (import ../../network/ethernet.nix))
-  (lib.mkIf (cfg == "adhoc-wifi") (import ../../network/adhoc-wifi.nix))
+  (lib.mkIf (cfg == "adhoc-wifi") (import ../../network/adhoc-wifi.nix { inherit config lib; }))
   (lib.mkIf (cfg == "wifi-lan") (
     import ../../network/lan-wifi.nix {
       inherit config lib;
