0b1c095faca716ad8fb076c4da12e226b85b954e
Vladimir Timofeenko <id@vtimofeenko.com>
2023-08-21 13:50:00 -0700

    [zsh] fix: correct import of home-manager into user
    This fix is needed to use the correct env variables as the user (SSH_AUTH_SOCK, PASSWORD_STORE_DIR, etc.)

--- modules/user/default.nix
+++ modules/user/default.nix
@@ -3,6 +3,7 @@
 , config
 , lib
 , my-doom-config
+, nixpkgs-unstable
 , ...
 }:
 {
@@ -16,23 +17,28 @@
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
 
-  home-manager.users.spacecadet = { ... }: {
-    imports = [
-      my-doom-config.nixosModules.default
-      ../home-manager # (ref:linux-user-import)
-    ];
-    home.packages = builtins.attrValues {
-      inherit (pkgs) pavucontrol blueman libreoffice firefox brave gthumb;
+  home-manager.users.spacecadet = { ... }:
+    let
+      zsh-module = import ../home-manager/zsh { inputs = { inherit nixpkgs-unstable; }; inherit config pkgs; };
+    in
+    {
+      imports = [
+        my-doom-config.nixosModules.default
+        ../home-manager # (ref:linux-user-import)
+        zsh-module
+      ];
+      home.packages = builtins.attrValues {
+        inherit (pkgs) pavucontrol blueman libreoffice firefox brave gthumb;
+      };
+
+      programs.browserpass.enable = true;
+
+      programs.password-store = {
+        enable = true;
+        package = pkgs.pass.withExtensions (exts: [ exts.pass-otp ]);
+      };
+
+      home.file.".icons/default".source = "${pkgs.vanilla-dmz}/share/icons/Vanilla-DMZ";
     };
-
-    programs.browserpass.enable = true;
-
-    programs.password-store = {
-      enable = true;
-      package = pkgs.pass.withExtensions (exts: [ exts.pass-otp ]);
-    };
-
-    home.file.".icons/default".source = "${pkgs.vanilla-dmz}/share/icons/Vanilla-DMZ";
-  };
 }
 # NixOS user configs:1 ends here
