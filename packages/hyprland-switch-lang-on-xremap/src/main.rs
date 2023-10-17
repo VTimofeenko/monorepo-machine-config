f1ddbbfb4e682ccda1e5db21b90ad13d4135f0e2
Vladimir Timofeenko <id@vtimofeenko.com>
2023-10-17 12:09:11 -0700

    [pkgs] hyprland switch lang wrapper: adjust xremap device detect logic
    As of the latest versions xremap no longer calls the device xremap-pid-... by default

--- packages/hyprland-switch-lang-on-xremap/src/main.rs
+++ packages/hyprland-switch-lang-on-xremap/src/main.rs
@@ -5,9 +5,9 @@ use hyprland::ctl::*;
 fn main() -> hyprland::Result<()> {
     let devices = Devices::get()?;
     // Very naive:
-    // 1. Relies on not documented fact that xremap creates a keyboard with the name "xremap-pid=$PID"
+    // 1. Relies on not documented fact that xremap creates a keyboard with the name "xremap"
     // 2. Relies on the fact that one and only one xremap exists
-    let binding = devices.keyboards.iter().filter(|device| device.name.starts_with("xremap-pid")).collect::<Vec<_>>();
+    let binding = devices.keyboards.iter().filter(|device| device.name.starts_with("xremap")).collect::<Vec<_>>();
     let xremap_device = match &binding.first() {
         Some(x) => &x.name,
         None => panic!("Could not find xremap device"),
