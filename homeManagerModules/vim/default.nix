2e72dc865c4e4cc6b045b0b163ccaf46deaa52e1
Vladimir Timofeenko <id@vtimofeenko.com>
2023-10-26 10:45:09 -0700

    [vim] feat: add custom levels of notifications for todo-comments

--- homeManagerModules/vim/default.nix
+++ homeManagerModules/vim/default.nix
@@ -49,8 +49,50 @@ in
             type = "lua";
             config = # lua
               ''
+                -- The custom configuration maps the todo-comments to my usual levels of urgency
                 local tc = require("todo-comments")
+                local function shallowcopy(orig)
+                  local orig_type = type(orig)
+                  local copy
+                  if orig_type == 'table' then
+                     copy = {}
+                     for orig_key, orig_value in pairs(orig) do
+                         copy[orig_key] = orig_value
+                     end
+                  else -- number, string, boolean, etc
+                       copy = orig
+                  end
+                  return copy
+                end
+
                 tc.setup()
+
+                local levelMap = {
+                    -- Three level urgency system
+                    ["Info"] = { "Todo", "Note" },
+                    ["Warn"] = { "Warn", "Perf", "Test" },
+                    ["Error"] = { "Fix", "Hack" }
+                }
+
+                -- override the links
+                for key in pairs(levelMap) do
+                    local diagnosticHlGroup = 'Diagnostic' .. key -- -> 'DiagnosticInfo', etc.
+
+                    for _, todoCommentsGroup in ipairs(levelMap[key]) do
+                        local groupText = vim.api.nvim_get_hl(0, { name = diagnosticHlGroup })
+                        groupText["italic"] = true -- to separate from normal comments
+                        local groupBadge = shallowcopy(groupText)
+                        groupBadge["reverse"] = true
+
+                        -- first the sign
+                        vim.api.nvim_set_hl(0, 'TodoSign' .. todoCommentsGroup, { link = diagnosticHlGroup })
+                        vim.api.nvim_set_hl(0, 'TodoFg' .. todoCommentsGroup, groupText)
+                        vim.api.nvim_set_hl(0, 'TodoBg' .. todoCommentsGroup, groupBadge)
+                    end
+                end
+
+                -- clear treesitter's comment highlight
+                vim.api.nvim_set_hl(0, 'Todo', {})
 
                 vim.keymap.set("n", "]t", function()
                   tc.jump_next()
