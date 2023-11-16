5c856ab1d72a89fc56365a7b39b0fb2035bb0fde
Vladimir Timofeenko <id@vtimofeenko.com>
2023-11-16 15:22:28 -0800

    fix: remove garbage message from logs

--- modules/hardware/keyboard.nix
+++ modules/hardware/keyboard.nix
@@ -2,8 +2,8 @@
 { lib, pkgs, ... }: {
   # taken from https://github.com/dygmalab/bazecor/blob/159eed1d37f3fd1fbf5c17023c12bb683b778281/src/main/index.js#l223
   services.udev.extraRules = ''
-    subsystems=="usb", attrs{idvendor}=="1209", attrs{idproduct}=="2201", group="users", mode="0666"
-    subsystems=="usb", attrs{idvendor}=="1209", attrs{idproduct}=="2200", group="users", mode="0666"
+    subsystem=="usb", attrs{idvendor}=="1209", attrs{idproduct}=="2201", group="users", mode="0666"
+    subsystem=="usb", attrs{idvendor}=="1209", attrs{idproduct}=="2200", group="users", mode="0666"
   '';
 }
 # Keyboard:1 ends here
