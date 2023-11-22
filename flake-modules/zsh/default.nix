80a7472c69217b51b5ea6fdda1ffc8f31bbba269
Vladimir Timofeenko <id@vtimofeenko.com>
2023-11-21 20:52:39 -0800

    chore: todo

--- flake-modules/zsh/default.nix
+++ flake-modules/zsh/default.nix
@@ -1,6 +1,7 @@
 # Flake module that exposes my zsh config as a home manager and NixOS modules
 { self, lib }:
 {
+  # TODO: fzf ignore lock files for completion unless it's the only option
   flake = {
     nixosModules.zsh = import ./nixosModule.nix { inherit self; };
     # System-wide settings that may needed if using the home-manager module
