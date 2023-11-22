4ef808b1ad13d8208d62d91f8b8ba801922b866a
Vladimir Timofeenko <id@vtimofeenko.com>
2023-11-22 15:33:17 -0800

    fix: correct udev syntax

--- modules/hardware/keyboard.nix
+++ modules/hardware/keyboard.nix
@@ -1,9 +1,9 @@
 # [[file:../../new_project.org::*Keyboard][Keyboard:1]]
-{ lib, pkgs, ... }: {
+_: {
   # taken from https://github.com/dygmalab/bazecor/blob/159eed1d37f3fd1fbf5c17023c12bb683b778281/src/main/index.js#l223
   services.udev.extraRules = ''
-    subsystem=="usb", attrs{idvendor}=="1209", attrs{idproduct}=="2201", group="users", mode="0666"
-    subsystem=="usb", attrs{idvendor}=="1209", attrs{idproduct}=="2200", group="users", mode="0666"
+    SUBSYSTEM=="usb", ATTR{idvendor}=="1209", ATTR{idproduct}=="2201", GROUP="users", MODE="0666"
+    SUBSYSTEM=="usb", ATTR{idvendor}=="1209", ATTR{idproduct}=="2200", GROUP="users", MODE="0666"
   '';
 }
 # Keyboard:1 ends here
