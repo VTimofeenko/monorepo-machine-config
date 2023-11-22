ac20ac4cbdf2480d7b92ba38da8eb9add01c55d9
Vladimir Timofeenko <id@vtimofeenko.com>
2023-11-22 10:27:50 -0800

    [vim] feat: make "look around" function automatically get the current file directory

--- flake-modules/vim/packages/default.nix
+++ flake-modules/vim/packages/default.nix
@@ -77,7 +77,7 @@ let
           local wk = require("which-key")
           wk.register({
             ["<leader>"] = { function() require'telescope.builtin'.find_files({cwd=vim.env.PRJ_ROOT, path_display = {"truncate"}}) end, "Find files in project" },
-            l = { function() require'telescope.builtin'.find_files() end, "Look around in the current dir" },
+            l = { function() require'telescope.builtin'.find_files({cwd=vim.fn.expand('%:h')}) end, "Look around in the current dir" },
             ["b"] = { require'telescope.builtin'.buffers, "Buffers" },
             ["/"] = { function() require'telescope.builtin'.live_grep({glob_pattern = "!*.lock"}) end, "Live grep" },
             f = {
