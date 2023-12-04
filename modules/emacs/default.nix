412823aad5d3736831c3e2e2a6c5494962b69cd6
Vladimir Timofeenko <id@vtimofeenko.com>
2023-12-03 19:47:58 -0800

    [emacs] chore: add desktop item

--- modules/emacs/default.nix
+++ modules/emacs/default.nix
@@ -13,7 +13,8 @@ let
   };
 
   doomGit = "https://github.com/doomemacs/doomemacs";
-  doomRepoLocation = "$XDG_CACHE_HOME/hm-doom-repo";
+  # Looks like desktopEntry requires this to be absolute
+  doomRepoLocation = "/home/spacecadet/.cache/hm-doom-repo";
   # Looks like tmpfiles wants this to be absolute
   # doomDir = "$XDG_CONFIG_HOME/doom";
   # gitManageddoomDir = "$HOME/code/literate-machine-config/modules/emacs/doom.dir";
@@ -35,19 +36,47 @@ in
     };
   };
 
+  xdg.desktopEntries = {
+    emacs = {
+      name = "Emacs";
+      exec = "emacs --init-directory ${doomRepoLocation} %F";
+      icon = "emacs";
+      mimeType = [
+        "text/english"
+        "text/plain"
+        "text/x-makefile"
+        "text/x-c++hdr"
+        "text/x-c++src"
+        "text/x-chdr"
+        "text/x-csrc"
+        "text/x-java"
+        "text/x-moc"
+        "text/x-pascal"
+        "text/x-tcl"
+        "text/x-tex"
+        "application/x-shellscript"
+        "text/x-c"
+        "text/x-c++"
+        ""
+      ];
+      settings.StartupWMClass = "Emacs";
+    };
+  };
+
   # TODO: add all icons font?
-  # TODO: patch the desktop
 
   /* This activation script will check out doom-emacs into a pre-defined directory
 
     It should check if the directory exist and become a no-op if it does ('true' part)
   */
+  # TODO: only if Linux
   home.activation.gitCheckoutDoom = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     $DRY_RUN_CMD [ ! -d ${doomRepoLocation} ] && ${lib.getExe pkgs.git} clone --depth=1 --single-branch "${doomGit}" "${doomRepoLocation} || true"
   '';
 
   # Doom really wants its dir in .config. I want to manage everything in this repo.
   # xdg.configFile."doom".source = config.lib.file.mkOutOfStoreSymlink gitManageddoomDir;
+  # TODO: only if Linux?
   systemd.user.tmpfiles.rules = [
     "L ${doomDir} - - - - ${gitManageddoomDir} "
   ];
