96cd4458549bbcb5d0309e9838af2ab373bf87e4
Vladimir Timofeenko <id@vtimofeenko.com>
2023-12-03 19:00:06 -0800

    [zsh] feat: add common XDG vars

--- flake-modules/zsh/common.nix
+++ flake-modules/zsh/common.nix
@@ -218,5 +218,9 @@ rec {
     EDITOR = "nvim";
     FZF_CTRL_T_COMMAND = "${getExe pkgs.fd} .";
     FZF_ALT_C_COMMAND = "${getExe pkgs.fd} -t d .";
+    XDG_CACHE_HOME = "$HOME/.cache";
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_DATA_HOME = "$HOME/.local/share";
+    XDG_STATE_HOME = "$HOME/.local/state";
   };
 }
