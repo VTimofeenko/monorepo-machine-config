074e0b83e830f32652d6963c4d15a9d77bb108b9
Vladimir Timofeenko <id@vtimofeenko.com>
2023-12-19 11:39:44 -0800

    [homelab] add cerium to this flake

---
+++ nixosConfigurations/cerium/configuration/default.nix
@@ -0,0 +1,35 @@
+{ lib, ... }:
+{
+  /* Boot */
+  boot = {
+    loader = {
+      grub = {
+        enable = true;
+        device = "/dev/sda";
+      };
+    };
+    initrd = {
+      availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
+    };
+    kernelModules = [ "kvm-intel" ];
+  };
+
+  /* File systems */
+  fileSystems."/" =
+    {
+      device = "/dev/disk/by-label/nixos";
+      fsType = "ext4";
+    };
+
+  swapDevices =
+    [{ device = "/dev/disk/by-label/swap"; }];
+
+  /* Misc */
+  system.stateVersion = "21.11";
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkForce true;
+  networking.interfaces.ens3.useDHCP = true;
+  /* Imports */
+  imports = [ ];
+}
+
