61e15c11777bdb9a4b3239789679ffc9d77a625d
Vladimir Timofeenko <id@vtimofeenko.com>
2024-01-13 12:27:50 -0800

    [homelab] feat: add misc icons to filedump

--- nixosModules/services/filedump/filedump.nix
+++ nixosModules/services/filedump/filedump.nix
@@ -2,11 +2,28 @@
 { lib, config, pkgs, ... }:
 let
   cfg = config.services.myFiledump;
+
+  misc-icons = pkgs.stdenv.mkDerivation {
+    name = "misc-icons";
+    src = pkgs.fetchurl {
+      url = "https://downloads.filestash.app/brand/logo_white.svg";
+      hash = "sha256-ecpNz9I91ARQ8cepGRJthINO0BsKOgfAge3mUph+O0M=";
+    };
+
+    dontUnpack = true;
+    dontBuild = true;
+
+    installPhase = ''
+      mkdir -p $out/share/icons
+      cp $src $out/share/icons/filestash_logo_white.svg
+    '';
+  };
 in
 {
   options.services.myFiledump.dir = lib.mkOption { type = lib.types.path; default = "/var/lib/filedump"; };
   config.systemd.tmpfiles.rules = [
     "d ${cfg.dir} 0755 root root"
     "L ${cfg.dir}/dashboard-icons - - - - ${pkgs.dashboard-icons}"
+    "L ${cfg.dir}/misc-icons - - - - ${misc-icons}/share/icons"
   ];
 }
