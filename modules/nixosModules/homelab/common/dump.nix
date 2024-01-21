674445831d230ab420fa810ab29a21c6b0c04b10
Vladimir Timofeenko <id@vtimofeenko.com>
2024-01-21 12:01:33 -0800

    chore: keep logs in RAM so that the emmc is not hammered with writes

--- modules/nixosModules/homelab/common/dump.nix
+++ modules/nixosModules/homelab/common/dump.nix
@@ -1,3 +1,14 @@
 # Miscellaneous config setting dump
 _:
-{ sound.enable = false; }
+{
+  sound.enable = false;
+  /* Keeps the logs only in RAM
+
+    TODO: move this to proper log collection module when that's ready
+
+  */
+  services.journald.extraConfig = ''
+    Storage=volatile
+    RuntimeMaxUse=50M
+  '';
+}
