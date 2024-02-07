f94ec897bdb23f0e8e5903eac33761f73171579f
Vladimir Timofeenko <id@vtimofeenko.com>
2024-02-07 09:25:24 -0800

    [vim] chore: rework module generation to make simpler binding

--- flake-modules/vim/default.nix
+++ flake-modules/vim/default.nix
@@ -1,34 +1,36 @@
 # Flake module that exposes my custom neovim package and a module to configure it
 { withSystem, self }:
 {
-  # TODO: wl-clipboard <=> WAYLAND_DISPLAY is set
-  # TODO: Add lua shortcuts for trivial builders?
-  perSystem = { system, ... }: {
-    packages = withSystem system ({ inputs', ... }:
-      let
-        pkgBuilder = import ./packages;
-        commonArgs = {
-          pkgs = inputs'.nixpkgs-unstable.legacyPackages; # Vim is tracking the unstable packages
-          inherit (self) inputs;
-        };
-        vim = pkgBuilder commonArgs;
-        vimWithLangs = pkgBuilder (commonArgs // { enableLangServers = true; });
-      in
-      rec {
-        inherit vim vimWithLangs;
-        nvim = vim;
-        nvimWithLangs = vimWithLangs;
-        neovim = vim;
-        neovimWithLangs = vimWithLangs;
-      });
-  };
+  perSystem =
+    { system, ... }:
+    {
+      packages = withSystem system (
+        { inputs', ... }:
+        let
+          pkgBuilder = import ./packages;
+          commonArgs = {
+            pkgs = inputs'.nixpkgs-unstable.legacyPackages; # Vim is tracking the unstable packages
+            inherit (self) inputs;
+          };
+          vim = pkgBuilder commonArgs;
+          vimWithLangs = pkgBuilder (commonArgs // { enableLangServers = true; });
+        in
+        rec {
+          inherit vim vimWithLangs;
+          nvim = vim;
+          nvimWithLangs = vimWithLangs;
+          neovim = vim;
+          neovimWithLangs = vimWithLangs;
+        }
+      );
+    };
   flake =
     let
       # Both modules are very similar, so just build them using a "mode" flag below
-      moduleBuilder = import ./modules;
+      moduleBuilder = import ./modules self;
     in
     {
-      nixosModules.vim = moduleBuilder { localFlake = self; mode = "nixOS"; };
-      homeManagerModules.vim = moduleBuilder { localFlake = self; mode = "homeManager"; };
+      nixosModules.vim = moduleBuilder "nixOS";
+      homeManagerModules.vim = moduleBuilder "homeManager";
     };
 }
--- flake-modules/vim/modules/default.nix
+++ flake-modules/vim/modules/default.nix
@@ -1,12 +1,18 @@
-# NixOS module that configures my custom neovim package system-wide
-{ localFlake# Reference to the flake
-, mode# "homeManager" or "nixOS". Since the code is essentially the same, the difference is just a matter of paths
+# Produces NixOS or Home Manager module with the custom neovim
+localFlake: # Reference to the flake
+mode: # "homeManager" or "nixOS". Since the code is essentially the same, the difference is just a matter of paths
+
+{
+  pkgs,
+  config,
+  lib,
+  ...
 }:
-{ pkgs, config, lib, ... }:
 let
   inherit (lib) mkEnableOption mkIf;
   cfg = config.programs.myNvim;
   localPkgs' = localFlake.packages.${pkgs.stdenv.system};
+
   # Decide where to add the packages
   outer = if mode == "homeManager" then "home" else "environment";
   inner = if mode == "homeManager" then "packages" else "systemPackages";
@@ -17,11 +23,8 @@ in
     enable = mkEnableOption "My neovim with plugins";
     withLangServers = mkEnableOption "Enable language server plugins";
   };
-  config = mkIf cfg.enable {
-    ${outer}.${inner} =
-      # NOTE: symlinkJoin should help here
 
-      # assert assertMsg (!config.programs.neovim.enable) "This module is not compatible with the standard neovim module";
-      [ (if cfg.withLangServers then localPkgs'.vimWithLangs else localPkgs'.vim) ];
+  config = mkIf cfg.enable {
+    ${outer}.${inner} = [ (if cfg.withLangServers then localPkgs'.vimWithLangs else localPkgs'.vim) ];
   };
 }
