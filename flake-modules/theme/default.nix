8acc45e9605c8f0e51e665eb44e8a7aedc011f04
Vladimir Timofeenko <id@vtimofeenko.com>
2024-02-11 13:44:39 -0800

    [theme] feat: add 'data' attribute to flake output with the theme

--- flake-modules/theme/default.nix
+++ flake-modules/theme/default.nix
@@ -1,10 +1,9 @@
 # Flake module that produces NixOS and Home-manager modules with my theme
-{ self, ... }:
+{ self, lib, ... }:
 let
   module = import ./module.nix;
 in
 {
-  # TODO: add an attribute that prepends the "#" sign
   flake = {
     nixosModules.my-theme = {
       imports = [
@@ -18,5 +17,17 @@ in
         module
       ];
     };
+
+    data.my-colortheme =
+      (lib.evalModules {
+        modules = [
+          { _module.check = false; }
+          module
+        ];
+        specialArgs = {
+          inherit lib;
+          pkgs = self.inputs.nixpkgs-unstable.legacyPackages.x86_64-linux; # TODO: need a better way to pass pkgs here
+        };
+      }).config.my-colortheme;
   };
 }
