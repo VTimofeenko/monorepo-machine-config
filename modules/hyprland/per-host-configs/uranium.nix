4eb926a3188f50e6814fcdbf0d8c6ef012c6b074
Vladimir Timofeenko <id@vtimofeenko.com>
2024-02-20 13:43:16 -0800

    [hyprland] chore: remove unused sensitivity config

--- modules/hyprland/per-host-configs/uranium.nix
+++ modules/hyprland/per-host-configs/uranium.nix
@@ -3,10 +3,6 @@ _: {
   wayland.windowManager.hyprland = {
     extraConfig = ''
       monitor=eDP-1,2256x1504@60,0x0,1
-      # Touchpad that pretends to be a mouse?
-      device:frmw0001:00-32ac:0006-consumer-control-1 {
-        sensitivity = 1.0
-      }
     '';
   };
 }
