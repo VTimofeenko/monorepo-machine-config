e2a37f2102cb8a6fe486a1c18a202071a2c3b070
Vladimir Timofeenko <id@vtimofeenko.com>
2024-03-15 10:36:01 -0700

    [emacs] fix: use aspell instead of flyspell
    Flyspell seems to malfunction constantly restarting its process

--- flake-modules/emacs/doom.dir/init.el
+++ flake-modules/emacs/doom.dir/init.el
@@ -81,7 +81,7 @@
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
-       (spell +flyspell) ; tasing you for misspelling mispelling
+       (spell +aspell) ; tasing you for misspelling mispelling
        ;;grammar           ; tasing grammar mistake every you make
 
        :tools
