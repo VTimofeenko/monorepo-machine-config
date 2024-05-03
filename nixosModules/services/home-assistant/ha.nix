eddcb9a797be02ef8f834f1c27071cf187b611a9
Vladimir Timofeenko <id@vtimofeenko.com>
2024-05-03 16:05:04 -0700

    [homelab] ha: add kafka

--- nixosModules/services/home-assistant/ha.nix
+++ nixosModules/services/home-assistant/ha.nix
@@ -34,6 +34,7 @@ in
       "mqtt"
       "dhcp"
       "zha"
+      "apache_kafka" # Streaming the events out
     ] ++ srvConfig.components;
 
     # Build custom components. { } does not pass deps which is OK for now
