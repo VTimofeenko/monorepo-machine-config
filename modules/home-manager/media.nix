556e48061a576b608a45d3bcfff9b3841ec93bba
Vladimir Timofeenko <id@vtimofeenko.com>
2024-05-04 11:01:17 -0700

    mpv: add movement shortcuts

--- modules/home-manager/media.nix
+++ modules/home-manager/media.nix
@@ -5,6 +5,10 @@ _: {
     bindings = {
       WHEEL_UP = "add volume 5";
       WHEEL_DOWN = "add volume -5";
+      "Alt+h" = "add video-pan-x 0.05";
+      "Alt+l" = "add video-pan-x -0.05";
+      "Alt+k" = "add video-pan-y 0.05";
+      "Alt+j" = "add video-pan-y -0.05";
     };
     config = {
       hwdec = true;
