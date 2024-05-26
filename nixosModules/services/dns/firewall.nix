9ba9f85116bf5b147d5339b6e01730084a581f68
Vladimir Timofeenko <id@vtimofeenko.com>
2024-05-26 14:28:43 -0700

    dns: make firewall adapter-aware

--- nixosModules/services/dns/firewall.nix
+++ nixosModules/services/dns/firewall.nix
@@ -1,3 +1,18 @@
-_: {
-  networking.firewall.allowedUDPPorts = [ 53 ]; # TODO: Allow only on client and lan?
+# TODO: Add generic "allow all 53" back; check impact on DNS performance. Probably neglibible.
+{ lib, config, ... }:
+{
+  # Takes the list of networks the service should access and sets the firewall rules accordingly
+  networking.firewall.interfaces = lib.pipe (lib.homelab.getService "dns") [
+    (builtins.getAttr "networkAccess") # -> ["lan" "client"]
+    (map (
+      network: (lib.homelab.getHost config.networking.hostName).networks.${network}.adapter or network
+    )) # -> ["eth0" "client"]
+    (map (interface: {
+      name = interface;
+      value = {
+        allowedUDPPorts = [ 53 ];
+      };
+    }))
+    builtins.listToAttrs
+  ];
 }
