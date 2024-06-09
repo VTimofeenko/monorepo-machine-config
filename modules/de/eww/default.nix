cdac2bc9742c0d691b2b6ea6f5f2325d2a956766
Vladimir Timofeenko <id@vtimofeenko.com>
2024-06-09 09:56:39 -0700

    upgrade: rename eww-wayland -> eww

--- modules/de/eww/default.nix
+++ modules/de/eww/default.nix
@@ -1,6 +1,6 @@
 # [[file:../../../new_project.org::*Eww -- widgets][Eww -- widgets:1]]
 { pkgs, ... }:
 {
-  home.packages = [ pkgs.eww-wayland ]; # Enough for development, for now.
+  home.packages = [ pkgs.eww ]; # Enough for development, for now.
 }
 # Eww -- widgets:1 ends here
