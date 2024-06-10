bb1d6413043c9cd21ddbca72a4a9867b6de2b6f9
Vladimir Timofeenko <id@vtimofeenko.com>
2024-06-10 10:10:35 -0700

    homelab: start iscsi experiment with neutronium

--- nixosConfigurations/neutronium/default.nix
+++ nixosConfigurations/neutronium/default.nix
@@ -1,5 +1,10 @@
 # Config for my NixOS installer
-{ lib, config, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 let
   inherit (config) my-data;
 in
@@ -7,4 +12,17 @@ in
   # Force start SSH
   systemd.services.sshd.wantedBy = lib.mkForce [ "multi-user.target" ];
   users.users.root.openssh.authorizedKeys.keys = my-data.settings.SSHKeys;
+  services.openiscsi = {
+    name = "neutronium";
+    enable = true;
+  };
+  environment.systemPackages = [
+    pkgs.openiscsi
+    pkgs.libiscsi
+  ];
+
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = "experimental-features = nix-command flakes";
+  };
 }
