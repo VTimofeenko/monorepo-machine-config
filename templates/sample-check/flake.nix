a5ca58310f750cb53cf34f113508c25ee9f94aaf
Vladimir Timofeenko <id@vtimofeenko.com>
2024-06-14 09:32:35 -0700

    template: update to 24.05

--- templates/sample-check/flake.nix
+++ templates/sample-check/flake.nix
@@ -2,9 +2,9 @@
   description = "Sample check flake";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.11";
+    nixpkgs.url = "nixpkgs/nixos-24.05";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
