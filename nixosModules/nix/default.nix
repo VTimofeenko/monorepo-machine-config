c5199490978d4e79b4c509f483f966d9291ab986
Vladimir Timofeenko <id@vtimofeenko.com>
2024-07-02 11:03:20 -0700

    nix: use settings.* instead of extraConfig

--- nixosModules/nix/default.nix
+++ nixosModules/nix/default.nix
@@ -29,16 +29,12 @@ in
   # Allow unfree packages across the board
   nixpkgs.config.allowUnfree = true;
   nix = {
-    extraOptions = ''
-      # Quicker timeout for inaccessible binary caches
-      connect-timeout = 5
-      # Enable flakes
-      experimental-features = nix-command flakes
-      # Do not warn on dirty git repo
-      warn-dirty = false
-      # Automatically optimize store
-      # auto-optimise-store = true
-    '';
+    settings = {
+      connect-timeout = 5;
+      experimental-features = "nix-command flakes";
+      warn-dirty = false;
+      auto-optimise-store = false;
+    };
     registry =
       {
         ns.flake = nixpkgs-stable;
