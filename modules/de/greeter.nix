f1bc74be8e7d1c88a53b2e4a52953ac30ff735ad
Vladimir Timofeenko <id@vtimofeenko.com>
2024-07-07 14:29:11 -0700

    chore: remove comments

--- modules/de/greeter.nix
+++ modules/de/greeter.nix
@@ -1,15 +1,9 @@
-# [[file:../../new_project.org::*System greeter][System greeter:1]]
 { pkgs, ... }:
 {
-  # System greeter:1 ends here
-  # [[file:../../new_project.org::*System greeter][System greeter:2]]
   boot.kernelParams = [ "console=tty1" ];
   services.greetd = {
     enable = true;
     vt = 2;
-    # System greeter:2 ends here
-    # [[file:../../new_project.org::*System greeter][System greeter:3]]
-    # TODO: Add launch-hyprland to user packages
     settings = {
       default_session = {
         command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd ${pkgs.zsh}/bin/zsh";
@@ -18,4 +12,3 @@
     };
   };
 }
-# System greeter:3 ends here
