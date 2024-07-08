c724ede831ad5636c76322ed11d9d16a6c65bffc
Vladimir Timofeenko <id@vtimofeenko.com>
2024-07-08 11:53:46 -0700

    vim: feat: make treesitter context less noisy

--- flake-modules/neovim/modules/configs/treesitter-context.lua
+++ flake-modules/neovim/modules/configs/treesitter-context.lua
@@ -1,10 +1,16 @@
 vim.api.nvim_set_hl(0, "TreesitterContextBottom", { underline = true })
 local wk = require("which-key")
+local tsContext = require("treesitter-context")
 
-wk.register({
-	u = { require("treesitter-context").go_to_context, "Jump to treesitter context" },
-}, { prefix = "<localleader>" })
+tsContext.setup({
+	max_lines = 4,
+	on_attach = function()
+		wk.register({
+			u = { tsContext.go_to_context, "Jump to treesitter context" },
+		}, { prefix = "<localleader>" })
 
-wk.register({
-	T = { c = { require("treesitter-context").toggle, "Toggle treesitter context" } },
-}, { prefix = "<leader>" })
+		wk.register({
+			T = { c = { tsContext.toggle, "Toggle treesitter context" } },
+		}, { prefix = "<leader>" })
+	end,
+})
