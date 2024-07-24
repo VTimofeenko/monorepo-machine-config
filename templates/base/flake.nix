86ac597d7ecb48d002a455d22c105391e18dd214
Vladimir Timofeenko <id@vtimofeenko.com>
2024-07-23 18:10:30 -0700

    template: chore: move to 24.05

--- templates/base/flake.nix
+++ templates/base/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixpkgs.follows = "nixpkgs-stable";
 
     devshell = {
