084d8cb9c69d5ef2e11fb5ed2b2f92a669e0a014
Vladimir Timofeenko <id@vtimofeenko.com>
2024-07-28 15:05:45 -0700

    homelab: feat: add actinium

---
+++ nixosConfigurations/actinium/configuration/default.nix
@@ -0,0 +1,39 @@
+{ lib, modulesPath, ... }:
+{
+  # Boot
+  boot = {
+    loader.grub = {
+      efiSupport = true;
+      efiInstallAsRemovable = true;
+      device = "nodev";
+    };
+    initrd = {
+      availableKernelModules = [
+        "ata_piix"
+        "uhci_hcd"
+        "xen_blkfront"
+        "vmw_pvscsi"
+      ];
+      kernelModules = [ "nvme" ];
+    };
+  };
+
+  # File systems
+  fileSystems = {
+    "/" = {
+      device = "/dev/sda1";
+      fsType = "ext4";
+      options = [ "noatime" ];
+    };
+    "/boot" = {
+      device = "/dev/disk/by-uuid/D5BC-D210";
+      fsType = "vfat";
+    };
+  };
+
+  # Misc
+  system.stateVersion = "24.05";
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  # Imports
+  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+}
