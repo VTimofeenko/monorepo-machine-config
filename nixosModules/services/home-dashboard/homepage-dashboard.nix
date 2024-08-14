22ae708595e4cbb13aa6e305756abe2c60fba1e8
Vladimir Timofeenko <id@vtimofeenko.com>
2024-08-14 10:07:39 -0700

    homelab: homepage-dashboard: move to new nix-managed config

--- nixosModules/services/home-dashboard/homepage-dashboard.nix
+++ nixosModules/services/home-dashboard/homepage-dashboard.nix
@@ -1,47 +1,20 @@
 # Tiny module that provides configuration for homepage dashboard
-{
-  pkgs,
-  config,
-  lib,
-  ...
-}:
+{ config, lib, ... }:
 let
   inherit (lib.homelab) getServiceConfig getService getSrvSecret;
 
   srvName = "home-dashboard";
   srvCfg = getServiceConfig srvName;
   srv = getService srvName;
-
-  srvDataDir = "/var/lib/homepage-dashboard";
-
-  components =
-    map
-      (name: {
-        inherit name;
-        pkg = pkgs.writeTextDir "share/${name}.yaml" (builtins.toJSON srvCfg.${name});
-      })
-      [
-        "services"
-        "settings"
-        "bookmarks"
-        "widgets"
-      ];
 in
 {
-  # Enable the service
-  services.homepage-dashboard.enable = true;
+  services.homepage-dashboard = {
+    enable = true;
+    inherit (srvCfg) bookmarks settings services;
+  };
+
   systemd.services.homepage-dashboard = {
     serviceConfig = {
-      /*
-        Configure the dashboard using information from my-data
-
-        Will overwrite all the yaml files that home-dashboard needs with symlinks ot paths in Nix store
-      */
-      ExecStartPost = map (
-        component:
-        "${pkgs.coreutils}/bin/ln --symbolic --force ${component.pkg}/share/${component.name}.yaml ${srvDataDir}"
-      ) components;
-
       # Use LoadCredential to inject the secrets into systemd unit. Works with DynamicUser which is what homepage-dashboard service uses.
       LoadCredential = lib.mapAttrsFlatten (
         name: _: "${name}:${config.age.secrets.${name}.path}"
