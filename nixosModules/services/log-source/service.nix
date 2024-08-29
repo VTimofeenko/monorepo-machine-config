f62b9212a1427952512223381b4e2def031bf488
Vladimir Timofeenko <id@vtimofeenko.com>
2024-08-29 16:59:37 -0700

    homelab: logging: add a separate topic writer for os deployment events

--- nixosModules/services/log-source/service.nix
+++ nixosModules/services/log-source/service.nix
@@ -23,6 +23,12 @@ in
           type = "journald";
           # exclude_matches._SYSTEMD_UNIT
         };
+        local-journal-nixos = {
+          type = "journald";
+          include_matches = {
+            SYSLOG_IDENTIFIER = [ "nixos" ];
+          };
+        };
       };
       # This table allows looking up IPs in the dataset and matching them to hostnames.
       enrichment_tables.my-data = {
@@ -54,7 +60,41 @@ in
           };
         };
       };
-      sinks = {
+
+      # This transformation parses nixos switch events in journald
+      # and emits filtered data with the target version and status
+      transforms.parse-nixos-events = {
+        type = "remap";
+        inputs = [ "local-journal-nixos" ];
+        source = # vrl
+          ''
+            # Skip the "starting" message
+            .message = string!(.message)
+            if contains(.message, "switching to") && !contains(.message, "(status") && !contains(.message, "finished") {
+                abort
+            }
+
+            # Extract target version and status of switch
+            # .*? is non-greedy match for hostname
+            target_version = parse_regex!(.message, r'.*nixos-system-.*?-(?P<version>.*)').version
+            status = parse_regex(.message, r'status (?P<status>\d)').status ?? 0
+
+            # Store needed values
+            host = .host
+            ts = .timestamp  # "timestamp" is reserved
+
+            # clear the object
+            del(.)
+
+            # Reconstruct the values
+            .target_version = target_version
+            .host = host
+            .timestamp = ts
+            .status = status
+          '';
+      };
+
+      sinks = rec {
         log-sink = {
           type = "kafka";
           inputs = [ "local-journal" ];
@@ -63,6 +103,10 @@ in
           # bootstrap_servers = "${service.fqdn}:9092"; # TODO: fix the network resolution in this vpn
           bootstrap_servers = "10.5.0.7:9092";
           topic = srvConfig.logTopic;
+        };
+        log-nixos-version-history = log-sink // {
+          inputs = [ "parse-nixos-events" ];
+          topic = srvConfig.deploymentsTopic;
         };
       };
     };
