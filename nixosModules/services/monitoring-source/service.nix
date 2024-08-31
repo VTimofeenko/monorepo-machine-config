b5c8a36f9dcb82c58fd8f2c9717771792781d077
Vladimir Timofeenko <id@vtimofeenko.com>
2024-08-31 11:26:30 -0700

    homelab: monitoring: add systemd ip accounting

--- nixosModules/services/monitoring-source/service.nix
+++ nixosModules/services/monitoring-source/service.nix
@@ -8,41 +8,48 @@ in
 {
   imports = [ ./collect-nixos-version.nix ];
 
-  services.prometheus.exporters =
+  # lib.mkMerge is effectively here:
+  # lib.foldl lib.recursiveUpdate { }
+  services.prometheus.exporters = lib.mkMerge [
     # Enable exporters with standard options
-    pipe
-      (pipe exporters [
-        (map (x: {
-          name = x;
-          value = {
-            enable = true;
-            inherit listenAddress;
-          };
-        }))
-        builtins.listToAttrs
-      ])
-      [
-        # not // to preserve enable and stuff
-        (lib.recursiveUpdate {
-          node = {
-            enabledCollectors = [
-              "conntrack"
-              "diskstats"
-              "filesystem"
-              "loadavg"
-              "meminfo"
-              "netdev"
-              "netstat"
-              "stat"
-              "time"
-              "systemd"
-              "hwmon"
-              "processes"
-            ];
-            extraFlags = [ "--collector.textfile.directory=/var/run/node_exporter_text" ];
-          };
-        })
+    #
+    (pipe exporters [
+      (map (x: {
+        name = x;
+        value = {
+          enable = true;
+          inherit listenAddress;
+        };
+      }))
+      builtins.listToAttrs
+    ])
+    {
+      node = {
+        enabledCollectors = [
+          "conntrack"
+          "diskstats"
+          "filesystem"
+          "loadavg"
+          "meminfo"
+          "netdev"
+          "netstat"
+          "stat"
+          "time"
+          "systemd"
+          "hwmon"
+          "processes"
+        ];
+        extraFlags = [ "--collector.textfile.directory=/var/run/node_exporter_text" ];
+      };
+    }
+    {
+      systemd.extraFlags = [
+        "--systemd.collector.enable-restart-count"
+        "--systemd.collector.enable-ip-accounting"
       ];
+
+    }
+  ];
 
   # W/a to allow nvme devices access
   # Source: https://github.com/NixOS/nixpkgs/issues/210041 and linked PRs
