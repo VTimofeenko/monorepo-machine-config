37bbf383732cd434b789fae7f15dae2151991b59
Vladimir Timofeenko <id@vtimofeenko.com>
2024-09-02 11:24:52 -0700

    break: roll back nvim because of treesitter spewing lua failures

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1&rev=bd526822deb9ed47c0b51b534817aa8541fff07b";
 
     nvim-nightly = {
-      url = "github:nix-community/neovim-nightly-overlay";
+      url = "github:nix-community/neovim-nightly-overlay?rev=e87be8db9b99173d375a9ba3b4fd3a57aaa7386e";
     };
 
     # Service that remaps arbitrary keyboard combinations
