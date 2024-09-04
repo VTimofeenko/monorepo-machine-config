bcae462d63deeeae2c5c4229cbe4b68879b6da3f
Vladimir Timofeenko <id@vtimofeenko.com>
2024-09-04 14:35:21 -0700

    homelab: ntp: monitoring: open firewall for scrape port

--- nixosModules/services/ntp/monitoring.nix
+++ nixosModules/services/ntp/monitoring.nix
@@ -1,7 +1,15 @@
 { config, lib, ... }:
+let
+  inherit (lib.homelab) getOwnIpInNetwork getServiceMonitoring;
+  srvName = "ntp";
+
+  inherit (getServiceMonitoring srvName) scrapePort;
+in
 {
   services.ntpd-rs.settings.observability.metrics-exporter-listen =
     assert lib.assertMsg config.services.ntpd-rs.enable "ntpd-rs needs to be enabled";
     assert lib.assertMsg config.services.ntpd-rs.metrics.enable "ntpd-rs metrics need to be enabled";
-    "${lib.homelab.getOwnIpInNetwork "monitoring"}:9975";
+    "${getOwnIpInNetwork "monitoring"}:${toString scrapePort}";
+
+  networking.firewall.interfaces.monitoring.allowedTCPPorts = lib.singleton scrapePort;
 }
