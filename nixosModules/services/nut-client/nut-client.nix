7b26879a341af97049a6fd7b2e30a4e58201acba
Vladimir Timofeenko <id@vtimofeenko.com>
2024-09-17 10:41:42 -0700

    homelab: nut: implement client

--- nixosModules/services/nut-client/nut-client.nix
+++ nixosModules/services/nut-client/nut-client.nix
@@ -1,16 +1,21 @@
-{ lib, pkgs, ... }:
-# TODO:
-# 0. Move password to secrets
-# 1. Connect to remote IP
-# 2. Configure actions explicitly (try on helium)
+{ lib, config, ... }:
 let
-  srvConfig = lib.homelab.getServiceConfig "nut-client";
+  # srvConfig = lib.homelab.getServiceConfig "nut-client";
+  inherit (lib.homelab) getServiceConfig getServiceSecret getServiceIP;
+  serverServiceName = "nut-server";
+  serverCfg = getServiceConfig serverServiceName;
 in
 {
+  age.secrets.nut-password.file = getServiceSecret "nut-client" "password";
+
   power.ups = {
-    users.upsmon = {
-      passwordFile = toString (pkgs.writeText "password" srvConfig.password);
-      upsmon = "master";
+    mode = "netclient"; # This will be overridden by mkForce "netserver" on the actual server
+
+    upsmon.monitor.${serverCfg.UPS.model} = {
+      user = if config.power.ups.mode == "netserver" then "primary-client" else "secondary-client";
+      system = "${serverCfg.UPS.model}@${getServiceIP serverServiceName}:3493";
+      passwordFile = config.age.secrets.nut-password.path;
     };
+
   };
 }
