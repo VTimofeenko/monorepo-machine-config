a3717914b1c9ba062c68141cc13c803910eb857c
Vladimir Timofeenko <id@vtimofeenko.com>
2024-09-19 08:35:34 -0700

    homelab: nut-client: implement upsscheduler dispatch script

--- nixosModules/services/nut-client/nut-client.nix
+++ nixosModules/services/nut-client/nut-client.nix
@@ -1,6 +1,5 @@
 { lib, config, ... }:
 let
-  # srvConfig = lib.homelab.getServiceConfig "nut-client";
   inherit (lib.homelab) getServiceConfig getServiceSecret getServiceIP;
   serverServiceName = "nut-server";
   serverCfg = getServiceConfig serverServiceName;
@@ -9,13 +8,68 @@ in
   age.secrets.nut-password.file = getServiceSecret "nut-client" "password";
 
   power.ups = {
+    enable = true;
     mode = "netclient"; # This will be overridden by mkForce "netserver" on the actual server
 
+    upsmon.settings = {
+      POWERDOWNFLAG = "/var/state/ups/killpower";
+      NOTIFYFLAG = [
+        [
+          "ONLINE"
+          "SYSLOG+WALL"
+        ]
+        [
+          "ONBATT"
+          "SYSLOG+WALL"
+        ]
+        [
+          "LOWBATT"
+          "SYSLOG+WALL"
+        ]
+        [
+          "FSD"
+          "SYSLOG+WALL"
+        ]
+        [
+          "COMMOK"
+          "EXEC"
+        ]
+        [
+          "COMMBAD"
+          "EXEC"
+        ]
+        [
+          "SHUTDOWN"
+          "SYSLOG+WALL"
+        ]
+        [
+          "REPLBATT"
+          "SYSLOG+WALL"
+        ]
+        [
+          "NOCOMM"
+          "SYSLOG+WALL+EXEC"
+        ]
+        [
+          "NOPARENT"
+          "SYSLOG+WALL"
+        ]
+      ];
+    };
     upsmon.monitor.${serverCfg.UPS.model} = {
       user = if config.power.ups.mode == "netserver" then "primary-client" else "secondary-client";
       system = "${serverCfg.UPS.model}@${getServiceIP serverServiceName}:3493";
       passwordFile = config.age.secrets.nut-password.path;
+      type =
+        let
+          match = {
+            netserver = "primary";
+            netclient = "secondary";
+          };
+        in
+        match.${config.power.ups.mode};
     };
 
   };
+  imports = [ ./upssched.nix ];
 }
---
+++ nixosModules/services/nut-client/upssched.nix
@@ -0,0 +1,74 @@
+{ pkgs, lib, ... }:
+let
+  # How many seconds the system should wait for the server/UPS to come back
+  systemGraceTime = "120";
+
+  upssched-dispatch = pkgs.writeShellApplication {
+    name = "upssched-dispatch";
+    runtimeInputs = [ pkgs.logger ];
+    text =
+      # bash
+      ''
+        # This script should be called by upssched via the CMDSCRIPT directive.
+        # The first argument passed to your CMDSCRIPT is the name of the timer from your AT lines or the value of EXECUTE directive
+        case $1 in
+          halt)
+        		logger -t upssched-dispatch "Got the halt event"
+            ${pkgs.systemd}/bin/shutdown now
+            ;;
+          *)
+        		logger -t upssched-dispatch "Unrecognized command: $1"
+        		;;
+        esac
+      '';
+  };
+in
+{
+  power.ups.schedulerRules =
+    lib.pipe
+      # help:
+      # https://networkupstools.org/docs/man/upssched.conf.html
+      ''
+        CMDSCRIPT ${lib.getExe upssched-dispatch}
+
+        PIPEFN /var/state/ups/upssched.pipe
+        LOCKFN /var/state/ups/upssched.lock
+        # Syntax:
+        # AT <notifyType> <upsName> <command>
+        AT ONLINE * CANCEL-TIMER halt
+
+        # If UPS is on battery -- start countdown timer and die
+        AT ONBATT * START-TIMER halt ${systemGraceTime}
+        # Just halt on low battery
+        AT LOWBATT * EXECUTE halt
+        # Halt on forced shutdown
+        AT FSD * EXECUTE halt
+
+        # Comms restored -- cancel the timer just in case
+        AT COMMOK * CANCEL-TIMER halt
+        # If communication to the server is lost -- start countdown timer and die
+        AT COMMBAD * START-TIMER halt ${systemGraceTime}
+
+        # Do nothing, this will be caught by monitoring
+        AT REPLBATT * EXECUTE REPLBATT
+
+        AT NOCOMM * START-TIMER halt ${systemGraceTime}
+
+        # This will log the state as unknown command
+        # I might want to catch this using monitoring
+        AT NOPARENT * EXECUTE NOPARENT
+        AT CAL * EXECUTE CAL
+        AT NOTCAL * EXECUTE NOTCAL
+        AT OFF * EXECUTE OFF
+        AT NOTOFF * EXECUTE NOTOFF
+        AT BYPASS * EXECUTE BYPASS
+        AT NOTBYPASS * EXECUTE NOTBYPASS
+        AT SUSPEND_STARTING * EXECUTE SUSPEND_STARTING
+        AT SUSPEND_FINISHED * EXECUTE SUSPEND_FINISHED
+      ''
+      [
+        (pkgs.writeText "upssched.conf")
+        toString
+      ];
+
+}
