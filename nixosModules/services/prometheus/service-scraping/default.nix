de1f197614e0aec6e466731f5a8d9da62b43e5d7
Vladimir Timofeenko <id@vtimofeenko.com>
2024-09-30 13:54:21 -0700

    prometheus: add bearer_token_file logic

--- nixosModules/services/prometheus/service-scraping/default.nix
+++ nixosModules/services/prometheus/service-scraping/default.nix
@@ -1,11 +1,30 @@
 # Scrapes the individual services metrics
 { lib, config, ... }:
 let
-  inherit (lib.homelab) getHostIpInNetwork getServiceMonitoring;
+  inherit (lib.homelab) getHostIpInNetwork getServiceMonitoring getServiceSecrets;
+  inherit (lib)
+    pipe
+    filterAttrs
+    mapAttrs'
+    nameValuePair
+    removeSuffix
+    ;
+
+  # Attrset of srvName = { pathToSecret, nameOfAgeSecretAttribute };
+  scrapeSecrets = pipe (getServiceSecrets "prometheus") [
+    (filterAttrs (n: _: lib.hasSuffix "scrape-token" n)) # -> only some secrets. Relies on string parsing which is bad
+    (mapAttrs' (
+      n: v:
+      nameValuePair (removeSuffix "-scrape-token" n) {
+        pathToSecret = v.materialFile;
+        nameOfAgeSecretAttribute = n;
+      }
+    ))
+  ];
 in
 {
   services.prometheus.scrapeConfigs = lib.pipe config.my-data.services.all [
-    (lib.filterAttrs (_: v: v ? monitoring && v.monitoring.enable && v.monitoring ? scrapeUrl))
+    (filterAttrs (_: v: v ? monitoring && v.monitoring.enable && v.monitoring ? scrapeUrl))
     # Extract the service <> scrapeUrl
     # Implementation note: technically all values don't need these functions but this will survive interface
     # changes
@@ -33,6 +52,7 @@ in
                 in
                 if x == null then "${ipAddress}:${toString scrapePort}" else x
               )
+              # Needs to be a list
               lib.singleton
             ];
             labels.nodeName = "${v.onHost}.home.arpa";
@@ -41,9 +61,24 @@ in
         metrics_path =
           v.monitoring.scrapeUrl
             or config.services.prometheus.exporters.${v.monitoring.exporterNixOption}.telemetryPath;
-        bearer_token = (lib.homelab.getServiceConfig srvName).metricsToken or "";
+        bearer_token = (lib.homelab.getServiceConfig srvName).metricsToken or null;
+        bearer_token_file =
+          if builtins.hasAttr srvName scrapeSecrets then
+            config.age.secrets."${scrapeSecrets.${srvName}.nameOfAgeSecretAttribute}".path
+          else
+            null;
       }
     ))
     builtins.attrValues
   ];
+
+  age.secrets = mapAttrs' (
+    _: v:
+    nameValuePair v.nameOfAgeSecretAttribute {
+      file = v.pathToSecret;
+      owner = config.users.users.prometheus.name; # This will fail if prometheus is disabled
+    }
+  ) scrapeSecrets;
+
+  services.prometheus.checkConfig = "syntax-only"; # bearer_token_file does not work with this
 }
