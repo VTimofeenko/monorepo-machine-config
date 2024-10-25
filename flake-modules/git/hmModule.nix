0f6adc39a10b7428f16de61bf8de0042c7f03640
Vladimir Timofeenko <id@vtimofeenko.com>
2024-10-24 20:16:06 -0700

    feat: lazygit: add conventional commit template

--- flake-modules/git/hmModule.nix
+++ flake-modules/git/hmModule.nix
@@ -77,6 +77,112 @@ in
           optionsTextColor = [ "#${fg-main}" ];
         };
         git.paging.pager = "${getExe pkgs.diff-so-fancy}";
+        customCommands = [
+          {
+            command = "git commit --message '{{.Form.Type}}{{ if .Form.Scope }}({{ .Form.Scope }}){{ end }}{{.Form.Breaking}}: {{.Form.Message}}'";
+            context = "global";
+            description = "Create new conventional commit";
+            key = "<c-v>";
+            loadingText = "Creating conventional commit...";
+            prompts = [
+              {
+                key = "Type";
+                options = [
+                  {
+                    description = "Changes that affect the build system or external dependencies";
+                    name = "build";
+                    value = "build";
+                  }
+                  {
+                    description = "A new feature";
+                    name = "feat";
+                    value = "feat";
+                  }
+                  {
+                    description = "A bug fix";
+                    name = "fix";
+                    value = "fix";
+                  }
+                  {
+                    description = "Other changes that don't modify src or test files";
+                    name = "chore";
+                    value = "chore";
+                  }
+                  {
+                    description = "Changes to CI configuration files and scripts";
+                    name = "ci";
+                    value = "ci";
+                  }
+                  {
+                    description = "Documentation only changes";
+                    name = "docs";
+                    value = "docs";
+                  }
+                  {
+                    description = "A code change that improves performance";
+                    name = "perf";
+                    value = "perf";
+                  }
+                  {
+                    description = "A code change that neither fixes a bug nor adds a feature";
+                    name = "refactor";
+                    value = "refactor";
+                  }
+                  {
+                    description = "Reverts a previous commit";
+                    name = "revert";
+                    value = "revert";
+                  }
+                  {
+                    description = "Changes that do not affect the meaning of the code";
+                    name = "style";
+                    value = "style";
+                  }
+                  {
+                    description = "Adding missing tests or correcting existing tests";
+                    name = "test";
+                    value = "test";
+                  }
+                ];
+                title = "Type of change";
+                type = "menu";
+              }
+              {
+                initialValue = "";
+                key = "Scope";
+                title = "Scope";
+                type = "input";
+              }
+              {
+                key = "Breaking";
+                options = [
+                  {
+                    name = "no";
+                    value = "";
+                  }
+                  {
+                    name = "yes";
+                    value = "!";
+                  }
+                ];
+                title = "Breaking change";
+                type = "menu";
+              }
+              {
+                initialValue = "";
+                key = "Message";
+                title = "message";
+                type = "input";
+              }
+              {
+                body = "Are you sure you want to commit?";
+                key = "Confirm";
+                title = "Commit";
+                type = "confirm";
+              }
+            ];
+          }
+        ];
       };
     };
   };
