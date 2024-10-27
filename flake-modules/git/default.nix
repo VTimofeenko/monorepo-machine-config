0a4fa67f4bd3b538dbf80e88524d17d993d0012f
Vladimir Timofeenko <id@vtimofeenko.com>
2024-10-27 10:10:55 -0700

    refactor(git): move home manager module into a dedicated dir

--- flake-modules/git/default.nix
+++ flake-modules/git/default.nix
@@ -1,2 +1,2 @@
 # Flake module that exposes my git configuration as a module
-_: { flake.homeManagerModules.git = import ./hmModule.nix; }
+_: { flake.homeManagerModules.git = import ./home-manager; }
