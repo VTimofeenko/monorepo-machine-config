7041a6a59cefcdce0464f7af8df6a47849c2bac9
Vladimir Timofeenko <id@vtimofeenko.com>
2024-10-29 10:53:42 -0700

    fix: correct rm usage in swayimg

--- modules/home-manager/swayimg/default.nix
+++ modules/home-manager/swayimg/default.nix
@@ -13,19 +13,25 @@
     enable = true;
     # FIXME: [24.11]
     package = nixpkgs-unstable.legacyPackages.${pkgs.system}.swayimg;
-    settings = {
-      # Disable showing image metadata (needs >2.2 in nixpkgs-stable) [24.11]
-      info.show = "no";
-      "keys.viewer" = {
-        n = "next_file";
-        p = "prev_file";
+    settings =
+      let
+        deleteCmd = "exec rm -- '%'; skip_file";
+      in
+      {
+        # Disable showing image metadata (needs >2.2 in nixpkgs-stable) [24.11]
+        info.show = "no";
+        "keys.viewer" = {
+          n = "next_file";
+          p = "prev_file";
+          "Shift+Delete" = deleteCmd;
+        };
+        "keys.gallery" = {
+          h = "step_left";
+          j = "step_down";
+          k = "step_up";
+          l = "step_right";
+          "Shift+Delete" = deleteCmd;
+        };
       };
-      "keys.gallery" = {
-        h = "step_left";
-        j = "step_down";
-        k = "step_up";
-        l = "step_right";
-      };
-    };
   };
 }
