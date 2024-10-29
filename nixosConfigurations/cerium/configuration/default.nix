8fa33500dd198bba4f195598060cbfcfeba22a56
Vladimir Timofeenko <id@vtimofeenko.com>
2024-10-28 19:55:40 -0700

    refactor(homelab): use networkd on cerium

--- nixosConfigurations/cerium/configuration/default.nix
+++ nixosConfigurations/cerium/configuration/default.nix
@@ -35,5 +35,7 @@
   hardware.cpu.intel.updateMicrocode = lib.mkForce true;
   networking.interfaces.ens3.useDHCP = true;
   # Imports
-  imports = [ ];
+  imports = [
+    ../../fluorine/configuration/experimental-networkd.nix
+  ];
 }
