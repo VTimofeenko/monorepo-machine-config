4f5747a6802abc8cb42b890345bb7f500e40a3a2
Vladimir Timofeenko <id@vtimofeenko.com>
2024-11-26 15:36:11 -0800

    feat(conventional-commit-helper): add showing scopes mode

--- flake-modules/git/home-manager/conventional-commit-helper/src/src/main.rs
+++ flake-modules/git/home-manager/conventional-commit-helper/src/src/main.rs
@@ -1,5 +1,7 @@
 use clap::{Parser, ValueEnum};
 use core::fmt;
+use git2::Repository;
+use regex::Regex;
 use serde::Deserialize;
 use std::path::Path;
 use std::{env, fs};
@@ -22,7 +24,7 @@ struct Args {
 /// This is a generic printable thing. The concrete examples would be:
 /// * Commit type
 /// * Commit scope
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 struct PrintableEntity {
     name: String,
     description: String,
@@ -66,12 +68,87 @@ fn get_types() -> Vec<PrintableEntity> {
     };
 }
 
+fn show_scopes() {
+    get_scopes(true).iter().for_each(|x| println!("{}", x));
+}
+
+/// Returns the possible commit scopes.
+/// Possible sources:
+/// * Per-project file in .dev/commit-scopes
+/// * Parsed from git commit history
+///
+/// Note that there is no default list of scopes -- they are per-project by definition.
+fn get_scopes(from_git_history: bool) -> Vec<PrintableEntity> {
+    let am_in_project: bool = env::var("PRJ_ROOT").is_ok();
+
+    let per_project_scopes = match am_in_project {
+        true => {
+            let project_commit_scope_file_path =
+                &format!("{}/.dev/commit-scopes.json", env::var("PRJ_ROOT").unwrap()).to_string();
+            let project_commit_scope_file = Path::new(project_commit_scope_file_path);
+
+            if project_commit_scope_file.exists() {
+                serde_json::from_str(&fs::read_to_string(project_commit_scope_file).unwrap())
+                    .unwrap()
+            } else {
+                Vec::new()
+            }
+        }
+        false => Vec::new(),
+    };
+
+    if from_git_history {
+        [per_project_scopes, get_scopes_from_commit_history()].concat()
+    } else {
+        per_project_scopes
+    }
+}
+
+fn get_scope_from_commit_message(message: &str) -> Option<String> {
+    // Regex to find the scope
+    // Typically scopes are found in the brackets:
+    // refactor(conventional-commit-helper): Change CommitType -> PrintableEntity to make it more generic
+    let scope_finder_regex = Regex::new(r"\w+\((.*)\).*").unwrap();
+
+    // The point is "Find the first match of a capture group. If it exists -- return it as Some.
+    // Otherwise return None"
+    // This is a bit ugly, there's probably a better way to destructure this.
+    let (_, [captured]) = match scope_finder_regex.captures_iter(message).next() {
+        Some(x) => x.extract(),
+        None => return None,
+    };
+
+    Some(captured.to_string())
+}
+
+/// Retrieves potential matches of scopes from the git history
+fn get_scopes_from_commit_history() -> Vec<PrintableEntity> {
+    let repo = match Repository::open(env::var("PRJ_ROOT").unwrap()) {
+        Ok(repo) => repo,
+        Err(e) => panic!("failed to open: {}", e),
+    };
+    let reflog = repo.reflog("HEAD").unwrap();
+
+    reflog
+        .iter()
+        .filter_map(|entry| get_scope_from_commit_message(entry.message().unwrap()))
+        // dedup by turning it into a hashset
+        .collect::<std::collections::HashSet<String>>()
+        .iter()
+        // Turn into needed structs
+        .map(|x| PrintableEntity {
+            name: x.to_string(),
+            description: "".to_string(),
+        })
+        .collect()
+}
+
 fn main() {
     let args = Args::parse();
 
     match args.mode {
         Mode::Type => show_types(),
-        Mode::Scope => todo!(),
+        Mode::Scope => show_scopes(),
     };
 }
 
@@ -105,5 +182,14 @@ mod tests {
     fn test_all_default_types_printed_as_expected(get_types_to_test: Vec<String>) {
         let re = regex::Regex::new(r"\w: .*").unwrap();
         assert!(get_types_to_test.iter().all(|x| re.is_match(x)))
+    }
+
+    /// Checks
+    ///
+    #[rstest]
+    #[case::present("foo(bar): baz", Some("bar".to_string()))]
+    #[case::absent("foo: baz", None)]
+    fn can_extract_scope_from_commit_msg(#[case] msg: &str, #[case] result: Option<String>) {
+        assert!(get_scope_from_commit_message(msg) == result)
     }
 }
