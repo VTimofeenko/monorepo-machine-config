b68bd165b37c03dee9dda239c3a696bab0bb43a1
Vladimir Timofeenko <id@vtimofeenko.com>
2024-12-15 12:01:51 -0800

    refactor(uranium): remove unused code
    The commented bits have remained in the files for long enough. They have
    been largely merged into nixos-hardware settings or have been fixed in
    the main module

--- nixosConfigurations/uranium/configuration/hardware/frame.work.nix
+++ nixosConfigurations/uranium/configuration/hardware/frame.work.nix
@@ -8,10 +8,6 @@
 {
   imports = [
     nixos-hardware.nixosModules.framework-13-7040-amd
-    # nixos-hardware.nixosModules.common-cpu-amd-pstate
-    # nixos-hardware.nixosModules.common-gpu-amd
-    # nixos-hardware.nixosModules.common-pc-laptop
-    # nixos-hardware.nixosModules.common-pc-laptop-ssd
   ];
   hardware = {
     enableRedistributableFirmware = true; # NOTE: required for wifi to work
@@ -19,15 +15,6 @@
       extraPackages = [ pkgs.rocmPackages.clr.icd ];
       enable32Bit = true;
     };
-
-    # Custom udev rules
-    # services.udev.extraRules = ''
-    #   # Fix headphone noise when on powersave
-    #   # https://community.frame.work/t/headphone-jack-intermittent-noise/5246/55
-    #   SUBSYSTEM=="pci", ATTR{vendor}=="0x8086", ATTR{device}=="0xa0e0", ATTR{power/control}="on"
-    #   # Ethernet expansion card support
-    #   ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="0bda", ATTR{idProduct}=="8156", ATTR{power/autosuspend}="20"
-    # '';
 
     # Mis-detected by nixos-generate-config
     # https://github.com/NixOS/nixpkgs/issues/171093
@@ -44,28 +31,8 @@
     kernelParams = [
       # Fixes white flickering after resume/unlock
       "amdgpu.sg_display=0"
-      # For Power consumption
-      # https://kvark.github.io/linux/framework/2021/10/17/framework-nixos.html
-
-      # "mem_sleep_default=deep"
-      # For Power consumption
-      # https://community.frame.work/t/linux-battery-life-tuning/6665/156
-      # Breaks AMD?
-      # "nvme.noacpi=1"
     ];
-
-    # Fix TRRS headphones missing a mic
-    # https://community.frame.work/t/headset-microphone-on-linux/12387/3
-    # extraModprobeConfig = ''
-    #   options snd-hda-intel model=dell-headset-multi
-    # '';
-
-    # For GPU support
-    # initrd.kernelModules = [ "amdgpu" ];
   };
-
-  # For fingerprint support
-  # services.fprintd.enable = lib.mkDefault true;
 
   services.xserver = {
     enable = true;
