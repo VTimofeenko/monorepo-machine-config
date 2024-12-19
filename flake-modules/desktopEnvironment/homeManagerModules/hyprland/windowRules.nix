fdb174b9413196fb257ba5237182fdc6f1b2bbff
Vladimir Timofeenko <id@vtimofeenko.com>
2024-12-18 16:22:41 -0800

    fix(desktop): improve window rules governing pinentry
    To better handle the idea of a "modal" at the top of the screen, I have
    reimplemented the old window rules as windowrulev2 which allows to
    somewhat reliably center a window at the top.

--- flake-modules/desktopEnvironment/homeManagerModules/hyprland/windowRules.nix
+++ flake-modules/desktopEnvironment/homeManagerModules/hyprland/windowRules.nix
@@ -1,17 +1,32 @@
 /**
   Home-manager module that configures per-window rules that don't have a better place to live.
 */
+let
+  settings.flyOut = {
+    classes = [
+      "pinentry-qt"
+      "xdg-desktop-portal-gtk"
+    ];
+    titles = [ "Open File" ];
+    tagName = "flyout";
+  };
+
+in
 {
   wayland.windowManager.hyprland.settings = {
-    "$pinentry" = "^(pinentry-qt)$";
-    "windowrule" = [
-      "float,$pinentry"
-      "float,xdg-desktop-portal-gtk"
-      "float,title:^Open File.*"
-      "size 25% 20%,$pinentry"
-      "center,$pinentry"
-      "stayfocused,$pinentry"
-      "dimaround,$pinentry"
-    ];
+    # "Flyout" is effectively a modal that should be created in the center top of the screen.
+    windowrulev2 =
+      # add tag to classes
+      (map (class: "tag +${settings.flyOut.tagName}, class:(${class})") settings.flyOut.classes)
+      # add tag to titles
+      ++ (map (title: "tag +${settings.flyOut.tagName}, title:(${title})") settings.flyOut.titles)
+      # implement the window rules for tag
+      ++ (map (rule: "${rule}, tag:${settings.flyOut.tagName}") [
+        "float"
+        "size 30% 20%"
+        "move 35% 10%"
+        "stayfocused"
+        "dimaround"
+      ]);
   };
 }
