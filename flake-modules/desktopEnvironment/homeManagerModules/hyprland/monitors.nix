fb4a90f316016a1df0ebaa5889ae01e8c9f0be60
Vladimir Timofeenko <id@vtimofeenko.com>
2024-12-26 11:28:27 -0800

    feat(desktop): add monitor shortcuts

--- flake-modules/desktopEnvironment/homeManagerModules/hyprland/monitors.nix
+++ flake-modules/desktopEnvironment/homeManagerModules/hyprland/monitors.nix
@@ -1,6 +1,38 @@
 # Hyprland monitor config
 { lib, ... }:
+let
+  srvLib = import ./binds/lib.nix { inherit lib; };
+
+  inherit (srvLib) mainMod Shift Alt Hyper;
+in
 {
+  wayland.windowManager.hyprland.myBinds = {
+    "Tab" = {
+      mod = mainMod;
+      dispatcher = "focusmonitor";
+      arg = "+1";
+      description = "focus the next monitor";
+    };
+    "${Shift}+Tab" = {
+      mod = mainMod;
+      dispatcher = "movewindow";
+      arg = "mon:+1";
+      description = "move current window to the next monitor";
+    };
+    "${Shift}+${Alt}+Tab" = {
+      mod = mainMod;
+      dispatcher = "movecurrentworkspacetomonitor";
+      arg = "+1";
+      description = "move current workpace to the next monitor";
+    };
+    "${Hyper}+Tab" = {
+      mod = mainMod;
+      dispatcher = "swapactiveworkspaces";
+      arg = "current +1";
+      description = "swap workspaces with the next monitor";
+    };
+  };
+
   wayland.windowManager.hyprland.settings.monitor = lib.mkDefault (
     builtins.trace
       ''You probably want to override the
