8760d03dd6852233f8561abcf58274fe200f90f3
Vladimir Timofeenko <id@vtimofeenko.com>
2025-01-15 09:26:58 -0800

    refactor(neovim): change the code to be more generic

--- flake-modules/neovim/config/default.nix
+++ flake-modules/neovim/config/default.nix
@@ -2,9 +2,10 @@
   Assembles the package configuration
 */
 { pkgs, lib, ... }:
-{
-  min =
-    ./minimal
+let
+  processFn =
+    _: x:
+    x
     |> lib.fileset.toList
     |> (map (it: import it { inherit pkgs; }))
     # Maybe: validate that there are no odd attrs in the elements of the list
@@ -13,6 +14,9 @@
       # Get either "plugin" attr or "plugins"
       plugins = (it |> builtins.catAttrs "plugin") ++ (it |> builtins.catAttrs "plugins" |> lib.flatten);
       initLua = it |> builtins.catAttrs "config" |> lib.concatStringsSep "\n";
-
     });
+in
+{
+  min = ./minimal;
 }
+|> builtins.mapAttrs processFn
