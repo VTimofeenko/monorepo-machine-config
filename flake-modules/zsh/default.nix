126f7d30105e7f27cf47058d5b420030acbf4e9f
Vladimir Timofeenko <id@vtimofeenko.com>
2025-01-28 18:15:27 -0800

    refactor(zsh): merge all code into the top file

--- flake-modules/zsh/default.nix
+++ flake-modules/zsh/default.nix
@@ -1,9 +1,22 @@
 # Flake module that exposes my zsh config as a home manager and NixOS modules
-{ self, ... }: {
+{ self, ... }:
+{
   flake = {
     # FIXME: ideally my theme should be imported here, but multiple imports of same module are broken
     # https://github.com/NixOS/nix/issues/7270
-    nixosModules.zsh = import ./modules/nixOS { inherit self; };
-    homeManagerModules.zsh = import ./modules/homeManager { inherit self;};
+    nixosModules.zsh =
+      { pkgs, lib, ... }:
+      {
+        imports = [
+          (import ./config { inherit lib pkgs self; }).nixosModule
+        ];
+      };
+    homeManagerModules.zsh =
+      { pkgs, lib, ... }:
+      {
+        imports = [
+          (import ./config { inherit lib pkgs self; }).homeManagerModule
+        ];
+      };
   };
 }
