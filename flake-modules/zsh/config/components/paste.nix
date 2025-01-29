ebd8dd26370e49fc4033428a8f74194c79e4b9cd
Vladimir Timofeenko <id@vtimofeenko.com>
2025-01-28 17:10:18 -0800

    refactor(zsh): factor out paste sttings

---
+++ flake-modules/zsh/config/components/paste.nix
@@ -0,0 +1,38 @@
+/**
+  Configures paste settings.
+*/
+{ lib, ... }:
+let
+  pasteSettings = [
+    # Escape URLs when pasting
+    ''
+      # Automatically escape urls when pasting
+      autoload -Uz url-quote-magic
+      zle -N self-insert url-quote-magic
+      autoload -Uz bracketed-paste-magic
+      zle -N bracketed-paste bracketed-paste-magic''
+    # Using zsh highlighting with long paths (like nix store) can be slow. This fixes it.
+    ''
+      # found https://gist.github.com/magicdude4eva/2d4748f8ef3e6bf7b1591964c201c1ab
+      pasteinit() {
+        OLD_SELF_INSERT=''${''${(s.:.)widgets[self-insert]}[2,3]}
+        zle -N self-insert url-quote-magic # I wonder if you'd need `.url-quote-magic`?
+      }
+
+      pastefinish() {
+        zle -N self-insert $OLD_SELF_INSERT
+      }
+      zstyle :bracketed-paste-magic paste-init pasteinit
+      zstyle :bracketed-paste-magic paste-finish pastefinish''
+  ];
+
+  init = pasteSettings |> lib.concatLines;
+in
+{
+  nixosModule = {
+    programs.zsh.interactiveShellInit = init;
+  };
+  homeManagerModule = {
+    programs.zsh.initExtra = init;
+  };
+}
--- flake-modules/zsh/modules/common/default.nix
+++ flake-modules/zsh/modules/common/default.nix
@@ -87,15 +87,6 @@ rec {
       # Add entry by "+" but do not exit menuselect
       bindkey -M menuselect "+" accept-and-menu-complete
     ''
-    # Escape URLs when pasting
-    ''
-      # Automatically escape urls when pasting
-      autoload -Uz url-quote-magic
-      zle -N self-insert url-quote-magic
-      autoload -Uz bracketed-paste-magic
-      zle -N bracketed-paste bracketed-paste-magic
-    ''
-
     # automatically open files with certain extensions in EDITOR
     (concatMapStringsSep "\n" (ext: "alias -s ${ext}=$EDITOR") [
       "nix"
@@ -119,20 +110,6 @@ rec {
           printf "%-32s %s\n" $command $completion
         done | sort | less
       }
-    ''
-    # Using zsh highlighting with long paths (like nix store) can be slow. This fixes it.
-    ''
-      # found https://gist.github.com/magicdude4eva/2d4748f8ef3e6bf7b1591964c201c1ab
-      pasteinit() {
-        OLD_SELF_INSERT=''${''${(s.:.)widgets[self-insert]}[2,3]}
-        zle -N self-insert url-quote-magic # I wonder if you'd need `.url-quote-magic`?
-      }
-
-      pastefinish() {
-        zle -N self-insert $OLD_SELF_INSERT
-      }
-      zstyle :bracketed-paste-magic paste-init pasteinit
-      zstyle :bracketed-paste-magic paste-finish pastefinish
     ''
   ];
   variables = {
