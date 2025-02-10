0fc346af5e5bd40e95168860a55d7657fc4d5dbc
Vladimir Timofeenko <id@vtimofeenko.com>
2025-02-09 19:20:00 -0800

    refactor(homelab): simplify nut-server firewall

--- nixosModules/services/nut-server/firewall.nix
+++ nixosModules/services/nut-server/firewall.nix
@@ -1,13 +1,9 @@
-{ config, lib, ... }:
+{ lib, ... }:
 let
-  inherit (fwConfig) mainTable lanChain;
   inherit (lib.homelab) getServiceConfig getService getHostIpInNetwork;
   nftConcat = lib.concatStringsSep ", ";
 
   srvName = "nut-server";
-  fwSrvName = "wan_firewall";
-
-  fwConfig = getServiceConfig fwSrvName;
 
   clientIPs = lib.pipe "nut-client" [
     getService
@@ -17,13 +13,8 @@ let
   ];
 in
 {
-  networking.nftables.ruleset =
-    assert (config.networking.hostName == (getService fwSrvName).onHost); # relies on the fact that this is running on router
-    ''
-      table inet ${mainTable} {
-        chain ${lanChain} {
-          tcp dport ${toString (getServiceConfig srvName).port} ip saddr { ${nftConcat clientIPs}} accept comment "NUT traffic"
-        }
-      }
-    '';
+  networking.myFirewall.chains.input-lan-br.rules = ''
+    tcp dport ${toString (getServiceConfig srvName).port} ip saddr { ${nftConcat clientIPs}} accept comment "NUT traffic"
+  '';
+
 }
