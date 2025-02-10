523dca603ca76fd4f28dadce8e8f949f1db20436
Vladimir Timofeenko <id@vtimofeenko.com>
2025-02-09 19:19:44 -0800

    refactor(homelab): simplify ntp firewall

--- nixosModules/services/ntp/firewall.nix
+++ nixosModules/services/ntp/firewall.nix
@@ -1,21 +1,3 @@
-{ config, ... }:
-let
-  inherit (config) my-data;
-
-  srvName = "ntp";
-  fwSrvName = "wan_firewall";
-
-  fwConfig = my-data.lib.getServiceConfig fwSrvName;
-in
 {
-  networking.nftables.ruleset =
-    assert (my-data.services.all.${srvName}.onHost == my-data.services.all.${fwSrvName}.onHost); # relies on the fact that this is running on router
-    with fwConfig;
-    ''
-      table inet ${mainTable} {
-        chain ${lanChain} {
-          udp dport 123 accept comment "NTP traffic"
-        }
-      }
-    '';
+  networking.firewall.interfaces.lan.allowedUDPPorts = [ 123 ];
 }
