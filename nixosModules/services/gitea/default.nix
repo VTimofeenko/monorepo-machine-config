66d60395a5cffcf6608cd76ee881fc2ed3947169
Vladimir Timofeenko <id@vtimofeenko.com>
2025-02-10 21:03:13 -0800

    refactor(homelab): move gitea to the new manifest format

--- nixosModules/services/gitea/default.nix
+++ nixosModules/services/gitea/default.nix
@@ -1,10 +1,3 @@
-# NixOS module that configures gitea
-_: {
-  imports = [
-    ./gitea.nix
-    ./ssl.nix
-    ./bkp.nix
-    ./monitoring.nix
-    ./network.nix
-  ];
+{
+  imports = (import ./manifest.nix).default;
 }
---
+++ nixosModules/services/gitea/manifest.nix
@@ -0,0 +1,38 @@
+let
+  serviceName = "gitea";
+in
+rec {
+  default = [
+    module
+    ingress.impl
+    # storage.impl
+    ./non-functional/ssl.nix
+    ./non-functional/bkp.nix
+    ./non-functional/network.nix
+    backups.impl
+  ];
+  module = ./gitea.nix;
+
+  ingress = {
+    impl = ./non-functional/firewall.nix;
+    # sslProxyConfig = ./non-functional/ssl.nix; # TODO: move to SSL proxy
+  };
+
+  monitoring = {
+    # TODO: implement here
+    impl = ./non-functional/monitoring.nix;
+  };
+  logging = false; # TODO: implement
+  storage = {
+    impl = ./non-functional/storage.nix;
+  };
+  backups = rec {
+    # FIXME: implement
+    enable = false;
+    schedule = "daily";
+    paths = [ ];
+    impl =
+      if enable then import ./non-functional/backups.nix { inherit paths schedule serviceName; } else { };
+    # TODO: remote!
+  };
+}
