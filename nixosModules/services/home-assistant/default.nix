902b7ec9a24036b6bb82019ee965455644bb72db
Vladimir Timofeenko <id@vtimofeenko.com>
2025-02-11 09:23:35 -0800

    feat(homelab): back up home-assistant

--- nixosModules/services/home-assistant/default.nix
+++ nixosModules/services/home-assistant/default.nix
@@ -1,9 +1,10 @@
-_: {
+{
   imports = [
     ./ha.nix
     ./zwave.nix
     ./ssl.nix
     ./outlet-rebooter.nix
     ./monitoring.nix
+    (import ./manifest.nix).backups.impl
   ];
 }
---
+++ nixosModules/services/home-assistant/manifest.nix
@@ -0,0 +1,36 @@
+let
+  serviceName = "home-assistant";
+in
+rec {
+  default = [
+    module
+    ingress.impl
+    storage.impl
+    ./non-functional/ssl.nix
+    ./non-functional/bkp.nix
+    backups.impl
+  ];
+  module = ./gitea.nix;
+
+  ingress = {
+    impl = ./non-functional/ssl.nix; # This file contains both SSL proxy and firewall
+    # sslProxyConfig = ./non-functional/ssl.nix; # TODO: move to SSL proxy
+  };
+
+  monitoring = {
+    # TODO: implement here
+    impl = ./non-functional/monitoring.nix;
+  };
+  logging = false; # TODO: implement
+  storage = {
+    impl = ./non-functional/storage.nix;
+  };
+  backups = rec {
+    enable = true;
+    schedule = "daily";
+    paths = [ "/var/lib/hass" ];
+    impl =
+      if enable then import ./non-functional/backups.nix { inherit paths schedule serviceName; } else { };
+    # TODO: remote!
+  };
+}
