87a55ffa0f190d4e5f0ea4482eb94d9a5e08b3a0
Vladimir Timofeenko <id@vtimofeenko.com>
2025-02-12 08:18:02 -0800

    fix(homelab): set restic server client max body size to accomodate bigger backups

--- nixosModules/services/restic-server/non-functional/ssl.nix
+++ nixosModules/services/restic-server/non-functional/ssl.nix
@@ -7,5 +7,9 @@
 self.serviceModules.ssl-proxy.srvLib.mkStandardProxyVHost {
   serviceName = "restic-server";
   port = 8080;
+  # Restic deals with big files
+  extraConfig = ''
+    client_max_body_size 500M;
+  '';
   inherit config lib;
 }
