c498954e1115dde5250dd4af59ee04b38594644f
Vladimir Timofeenko <id@vtimofeenko.com>
2025-02-17 09:11:04 -0800

    feat: add wpa_supplicant_gui

--- nixosConfigurations/uranium/configuration/network-selector.nix
+++ nixosConfigurations/uranium/configuration/network-selector.nix
@@ -1,5 +1,10 @@
 # [[file:../../../new_project.org::*Network selector][Network selector:1]]
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   cfg = config.myMachines.uranium.network;
   basePath = ../../../modules/network;
@@ -9,5 +14,6 @@ lib.mkMerge [
   (lib.mkIf (cfg == "eth") (import (basePath + /ethernet.nix)))
   (lib.mkIf (cfg == "adhoc-wifi") (import (basePath + /adhoc-wifi.nix) { inherit config lib; }))
   (lib.mkIf (cfg == "wifi-lan") (import (basePath + /lan-wifi.nix) { inherit config lib; }))
+  { environment.systemPackages = [ pkgs.wpa_supplicant_gui ]; }
 ]
 # Network selector:1 ends here
