d24a72ec0d3b0af850ecf8bbfdd6c8bba09147c2
Vladimir Timofeenko <id@vtimofeenko.com>
2025-03-18 15:36:06 -0700

    refactor(desktop): replace long list sum with pipes and flatten

--- flake-modules/desktopEnvironment/homeManagerModules/hyprland/windowRules.nix
+++ flake-modules/desktopEnvironment/homeManagerModules/hyprland/windowRules.nix
@@ -1,6 +1,7 @@
 /**
   Home-manager module that configures per-window rules that don't have a better place to live.
 */
+{ lib, ... }:
 let
   settings.flyOut = {
     classes = [
@@ -16,17 +17,19 @@ in
   wayland.windowManager.hyprland.settings = {
     # "Flyout" is effectively a modal that should be created in the center top of the screen.
     windowrulev2 =
-      # add tag to classes
-      (map (class: "tag +${settings.flyOut.tagName}, class:(${class})") settings.flyOut.classes)
-      # add tag to titles
-      ++ (map (title: "tag +${settings.flyOut.tagName}, title:(${title})") settings.flyOut.titles)
-      # implement the window rules for tag
-      ++ (map (rule: "${rule}, tag:${settings.flyOut.tagName}") [
-        "float"
-        "size 30% 20%"
-        "move 35% 10%"
-        "stayfocused"
-        "dimaround"
-      ]);
+      [
+        # Assign the tags
+        (settings.flyOut.classes |> map (class: "tag +${settings.flyOut.tagName}, class:(${class})"))
+        (settings.flyOut.titles |> map (title: "tag +${settings.flyOut.tagName}, title:(${title})"))
+        # Implement the tag window rules
+        (map (rule: "${rule}, tag:${settings.flyOut.tagName}") [
+          "float"
+          "size 30% 20%"
+          "move 35% 10%"
+          "stayfocused"
+          "dimaround"
+        ])
+      ]
+      |> lib.flatten;
   };
 }
