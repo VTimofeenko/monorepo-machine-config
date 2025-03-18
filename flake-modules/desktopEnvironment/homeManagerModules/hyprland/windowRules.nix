49d28a9d99622df12b361bc8e8b33a6b465e3284
Vladimir Timofeenko <id@vtimofeenko.com>
2025-03-18 15:39:45 -0700

    feat(desktop): add opensnitch prompt to flyout class

--- flake-modules/desktopEnvironment/homeManagerModules/hyprland/windowRules.nix
+++ flake-modules/desktopEnvironment/homeManagerModules/hyprland/windowRules.nix
@@ -21,6 +21,8 @@ in
         # Assign the tags
         (settings.flyOut.classes |> map (class: "tag +${settings.flyOut.tagName}, class:(${class})"))
         (settings.flyOut.titles |> map (title: "tag +${settings.flyOut.tagName}, title:(${title})"))
+        # OpenSnitch, but only the prompt
+        "tag +${settings.flyOut.tagName}, class:(opensnitch_ui), title:(OpenSnitch v.*)"
         # Implement the tag window rules
         (map (rule: "${rule}, tag:${settings.flyOut.tagName}") [
           "float"
