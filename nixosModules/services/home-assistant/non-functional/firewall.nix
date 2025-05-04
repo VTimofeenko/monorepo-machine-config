9b5be8f74d901f3aa80d3a0c7ea1166b7da81504
Vladimir Timofeenko <id@vtimofeenko.com>
2025-05-04 13:22:09 -0700

    refactor(homelab): use more generic functions to add trusted proxies to home assistant

--- nixosModules/services/home-assistant/non-functional/firewall.nix
+++ nixosModules/services/home-assistant/non-functional/firewall.nix
@@ -15,9 +15,7 @@
   services.home-assistant.config.http = {
     server_port = port;
     server_host = lib.homelab.getOwnIpInNetwork "backbone-inner";
-    trusted_proxies = [
-      (lib.homelab.getHostIpInNetwork "fluorine" "backbone-inner")
-    ];
+    trusted_proxies = lib.homelab.getSSLProxyIPs;
     use_x_forwarded_for = true;
   };
 }
