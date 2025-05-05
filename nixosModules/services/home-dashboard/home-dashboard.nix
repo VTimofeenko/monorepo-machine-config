c8e44693d83032bc882de661b0c1eb8af46f207b
Vladimir Timofeenko <id@vtimofeenko.com>
2025-05-05 10:52:17 -0700

    refactor(homelab): use proper function to generate imports from dir

--- nixosModules/services/home-dashboard/home-dashboard.nix
+++ nixosModules/services/home-dashboard/home-dashboard.nix
@@ -33,5 +33,5 @@ in
   # Add homepage-dashboard specific secrets to `agenix`
   age.secrets = builtins.mapAttrs (name: _: { file = getSrvSecret srvName name; }) srv.secrets;
 
-  imports = ./functional |> lib.fileset.fileFilter (file: file.hasExt "nix") |> lib.fileset.toList;
+  imports = lib.localLib.mkImportsFromDir ./functional;
 }
