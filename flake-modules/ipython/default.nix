39a0a1ad146d3198c7de64e4cfeefd4c2d6bd0d5
Vladimir Timofeenko <id@vtimofeenko.com>
2025-06-13 13:23:54 -0700

    feat(ipython): add ipython package
    Factoring out the ipython shell package allows using it as a package so
    it can be permanently installed

--- flake-modules/ipython/default.nix
+++ flake-modules/ipython/default.nix
@@ -1,42 +1,40 @@
 /**
-  Flake module that provides a wrapper around ipython with some commonly used libraries.
+  Flake module that provides a wrapper around `ipython` with some commonly used libraries.
 */
-{ withSystem, ... }:
-{
+_: {
   perSystem =
-    { system, ... }:
-    {
-      apps = withSystem system (
-        { inputs', ... }:
-        let
-          pkgs = inputs'.nixpkgs.legacyPackages;
+    { inputs', ... }:
+    let
+      pkgs = inputs'.nixpkgs.legacyPackages;
+      fzfHistorySearch =
+        builtins.readFile ./ipython-fzf.py
+        |> (it: {
+          text = it;
+          name = "ipython-fzf-history";
+        })
+        |> pkgs.writeTextFile;
 
-          fzfHistorySearch =
-            builtins.readFile ./ipython-fzf.py
-            |> (it: {
-              text = it;
-              name = "ipython-fzf-history";
-            })
-            |> pkgs.writeTextFile;
-        in
-        {
-          ipython = {
-            type = "app";
-            program = pkgs.writeShellApplication {
-              name = "ipython-wrapper";
-              runtimeInputs = [
-                (pkgs.python3.withPackages (python-pkgs: [
-                  python-pkgs.ipython
-                  python-pkgs.toolz
-                  python-pkgs.rich
-                  python-pkgs.pyfzf
-                ]))
-                pkgs.fzf
-              ];
-              text = ''ipython --TerminalInteractiveShell.editing_mode=vi -i "${fzfHistorySearch}" "$@"'';
-            };
-          };
-        }
-      );
+      ipython = pkgs.writeShellApplication {
+        name = "ipython-wrapper";
+        runtimeInputs = [
+          (pkgs.python3.withPackages (python-pkgs: [
+            python-pkgs.ipython
+            python-pkgs.toolz
+            python-pkgs.rich
+            python-pkgs.pyfzf
+          ]))
+          pkgs.fzf
+        ];
+        text = ''ipython --TerminalInteractiveShell.editing_mode=vi -i "${fzfHistorySearch}" "$@"'';
+      };
+    in
+    {
+      apps = {
+        ipython = {
+          type = "app";
+          program = ipython;
+        };
+      };
+      packages = { inherit ipython; };
     };
 }
