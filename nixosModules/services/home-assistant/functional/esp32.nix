68fd8f29cb902f5954c9db206dffdc3a79bb03ae
Vladimir Timofeenko <id@vtimofeenko.com>
2025-08-11 10:27:52 -0700

    feat(homelab): work around symlinks in esphome
    See nixpkgs #339557

--- nixosModules/services/home-assistant/functional/esp32.nix
+++ nixosModules/services/home-assistant/functional/esp32.nix
@@ -8,6 +8,7 @@
     downloaded by esphome was not executable. I fixed it by `chown` everything
     in `/var/lib/private/esphome` to `esphome:esphome`
 */
+{ lib, config, ... }:
 {
   services.home-assistant.extraComponents = [ "esphome" ];
 
@@ -15,6 +16,30 @@
     enable = true;
     allowedDevices = [
       "/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0"
+      "/dev/serial/by-id/usb-Espressif_USB_JTAG_serial_debug_unit_7C:2C:67:42:CD:C8-if00"
+      "/dev/serial/by-id/usb-1a86_USB_Single_Serial_58FA096927-if00"
     ];
   };
+
+  systemd.services.esphome =
+    let
+      inherit (lib) mkForce;
+
+      cfg = config.services.esphome;
+      stateDir = "/var/lib/private/esphome";
+      esphomeParams =
+        if cfg.enableUnixSocket then
+          "--socket /run/esphome/esphome.sock"
+        else
+          "--address ${cfg.address} --port ${toString cfg.port}";
+    in
+    {
+      environment.PLATFORMIO_CORE_DIR = mkForce "/var/lib/private/esphome/.platformio";
+
+      serviceConfig = {
+        ExecStart = mkForce "${cfg.package}/bin/esphome dashboard ${esphomeParams} ${stateDir}";
+        WorkingDirectory = mkForce stateDir;
+      };
+    };
+
 }
