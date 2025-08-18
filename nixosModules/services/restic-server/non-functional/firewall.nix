cf35901eae1353f657f169693a1d1a5d495424f8
Vladimir Timofeenko <id@vtimofeenko.com>
2025-08-18 11:46:50 -0700

    refactor(homelab): make restic listen on any IPs and firewall it off

--- nixosModules/services/restic-server/non-functional/firewall.nix
+++ nixosModules/services/restic-server/non-functional/firewall.nix
@@ -7,9 +7,4 @@
       ports = [ port ];
     })
   ];
-  services.restic.server.listenAddress = "${lib.homelab.getOwnIpInNetwork "backbone-inner"}:${port |> toString}";
-
-  # It's the _socket_ that needs to be ordered to start after network is up
-  systemd.sockets.restic-rest-server.requires = [ "network-online.target" ];
-  systemd.sockets.restic-rest-server.after = [ "network-online.target" ];
 }
