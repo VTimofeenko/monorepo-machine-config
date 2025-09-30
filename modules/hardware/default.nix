c0693568724516282cd6e95f9266e26b4621c774
Vladimir Timofeenko <id@vtimofeenko.com>
2025-09-30 09:40:08 -0700

    refactor: remove unneeded comments

--- modules/hardware/default.nix
+++ modules/hardware/default.nix
@@ -1,11 +1,9 @@
-# [[file:../../new_project.org::*Hardware][Hardware:1]]
 { ... }:
 {
   imports = [
-    ./disks.nix # (ref:disks-import)
-    ./scanner.nix # (ref:scanner-import)
-    ./printer.nix # (ref:printer-import)
-    ./keyboard.nix # (ref:keyboard-import)
+    ./disks.nix
+    ./scanner.nix
+    ./printer.nix
+    ./keyboard.nix
   ];
 }
-# Hardware:1 ends here
