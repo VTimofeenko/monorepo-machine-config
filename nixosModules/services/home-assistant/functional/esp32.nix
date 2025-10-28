VTimofeenko/monorepo-machine-config

commit b424f2f59acc3e91aa5d2cae662c89bb43d722a6

feat(homelab): use unstable for esphome

nixosModules/services/home-assistant/functional/esp32.nix
@@ -8,12 +8,13 @@
     downloaded by esphome was not executable. I fixed it by `chown` everything
     in `/var/lib/private/esphome` to `esphome:esphome`
 */
-{ lib, config, ... }:
+{ lib, config, pkgs-unstable, ... }:
 {
   services.home-assistant.extraComponents = [ "esphome" ];
 
   services.esphome = {
     enable = true;
+    package = pkgs-unstable.esphome;
     allowedDevices = [
       "/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0"
       "/dev/serial/by-id/usb-Espressif_USB_JTAG_serial_debug_unit_7C:2C:67:42:CD:C8-if00"

commit 415d5c521baa422c3d72b4979601048a2d9026e9

revert(homelab): use stable esphome
b424f2f59acc3e91aa5d2cae662c89bb43d722a6 caused devices to lose HA
integration. I will need to wait for 25.11 to do this again.

nixosModules/services/home-assistant/functional/esp32.nix
@@ -8,13 +8,16 @@
     downloaded by esphome was not executable. I fixed it by `chown` everything
     in `/var/lib/private/esphome` to `esphome:esphome`
 */
-{ lib, config, pkgs-unstable, ... }:
+{
+  lib,
+  config,
+  ...
+}:
 {
   services.home-assistant.extraComponents = [ "esphome" ];
 
   services.esphome = {
     enable = true;
-    package = pkgs-unstable.esphome;
     allowedDevices = [
       "/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0"
       "/dev/serial/by-id/usb-Espressif_USB_JTAG_serial_debug_unit_7C:2C:67:42:CD:C8-if00"