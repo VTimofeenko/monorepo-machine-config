623f88eece33d36d0f5ff263ee234ab197511892
Vladimir Timofeenko <id@vtimofeenko.com>
2025-11-13 10:58:50 -0800

    feat(homelab): implement alerts for gitea

--- nixosModules/services/gitea/manifest.nix
+++ nixosModules/services/gitea/manifest.nix
@@ -30,13 +30,30 @@ rec {
       path = "/metrics";
       impl = if enable then import ./non-functional/observability/metrics/impl.nix else { };
     };
+    logging = {
+      enable = true;
+      systemdUnit = "gitea.service";
+    };
+    alerts = {
+      enable = true;
+      Emergency = [ ];
+      Alert = [
+        # service availability
+        {
+          title = "service down";
+          query = "up{job=\"gitea-srv-scrape\"}";
+        }
+        # disk almost full
+        {
+          title = "disk almost full";
+          query = "(vector(0) and on() (((node_filesystem_avail_bytes{mountpoint=\"/var/lib/gitea\"} * 100) / node_filesystem_size_bytes{mountpoint=\"/var/lib/gitea\"}) < 10)) or on() vector(1)";
+        }
+        # TODO: (needs nginx metrics) error rate over X for a period
+        # TODO: (needs new DB) query duration
+      ];
+    };
   };
 
-  monitoring = {
-    # TODO: implement here
-    impl = ./non-functional/monitoring.nix;
-  };
-  logging = false; # TODO: implement
   storage = {
     impl = ./non-functional/storage.nix;
   };
