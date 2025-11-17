e1e2e1c5bd90334484732a74af42cc0cce2563b5
Vladimir Timofeenko <id@vtimofeenko.com>
2025-11-17 09:43:38 -0800

    refactor(homelab): redeploy actinium

--- nixosConfigurations/actinium/configuration/default.nix
+++ nixosConfigurations/actinium/configuration/default.nix
@@ -1,12 +1,11 @@
 { lib, modulesPath, ... }:
+let
+  hostConfig = lib.homelab.getOwnHostConfig;
+in
 {
   # Boot
   boot = {
-    loader.grub = {
-      efiSupport = true;
-      efiInstallAsRemovable = true;
-      device = "nodev";
-    };
+    loader.grub.device = "/dev/sda";
     initrd = {
       availableKernelModules = [
         "ata_piix"
@@ -25,14 +24,33 @@
       fsType = "ext4";
       options = [ "noatime" ];
     };
-    "/boot" = {
-      device = "/dev/disk/by-uuid/D5BC-D210";
-      fsType = "vfat";
+  };
+
+  # Network
+  systemd.network = {
+    links."10-wan" = {
+      enable = true;
+      linkConfig.Name = "wan";
+      matchConfig.PermanentMACAddress = hostConfig.macAddr;
+    };
+
+    networks."10-wan" = {
+      enable = true;
+      name = "10-wan";
+      matchConfig.Name = lib.mkForce "wan";
+      address = "${hostConfig.ipAddress}/${hostConfig.netmask}" |> lib.toList;
+      gateway = hostConfig.gateway |> lib.toList;
+      networkConfig = {
+        DHCP = "no";
+        # This will also disable IPv6 assigning
+        LinkLocalAddressing = "no";
+      };
+      DHCP = "no";
     };
   };
 
   # Misc
-  system.stateVersion = "24.05";
+  system.stateVersion = "25.05";
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   # Imports
   imports = [
