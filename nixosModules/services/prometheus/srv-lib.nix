d45fd947d527e501ea9142e3b8dc21396f995833
Vladimir Timofeenko <id@vtimofeenko.com>
2025-11-24 15:05:32 -0800

    feat(homelab): add port as keyword arg to prometheus mkBackboneInnerFirewallRules

--- nixosModules/services/prometheus/srv-lib.nix
+++ nixosModules/services/prometheus/srv-lib.nix
@@ -3,11 +3,22 @@
     Produce a standard firewall rule that allows Prometheus to scrape metrics.
   */
   mkBackboneInnerFirewallRules =
-    { lib, ports }:
+    {
+      lib,
+      ports ? null,
+      port ? null,
+    }:
+    let
+      ports' =
+        assert lib.assertMsg (lib.xor (isNull ports) (
+          isNull port
+        )) "`ports` and `port` cannot be specified together, but one of them must be specified.";
+        if !isNull ports then ports else port |> lib.toList;
+    in
     {
       # The validation is done by nftables, no need to make an extra check
       networking.firewall.extraInputRules =
-        ports
+        ports'
         # Turn into list just in case, so callers don't need to bother.
         |> lib.toList
         # Parse ports coming in as just int. If so – reconstruct attrset.
