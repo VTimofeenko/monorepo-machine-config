de8c997e7ea205a8c33e143a7e8d71b756a8d3e5
Vladimir Timofeenko <id@vtimofeenko.com>
2025-12-03 11:58:25 -0800

    feat(homelab): migrate to better NTP servers
    Node 3 in nixos pool seem to malfunction, causing in high ntp offsets

--- nixosModules/services/ntp/service.nix
+++ nixosModules/services/ntp/service.nix
@@ -1,11 +1,25 @@
 {
   services.ntpd-rs = {
     enable = true;
-    useNetworkingTimeServers = true;
+    useNetworkingTimeServers = false;
     settings = {
       server = [
         # Listen on all addresses, let `./firewall.nix` deal with it
         { listen = "0.0.0.0:123"; }
+      ];
+      source = [
+        {
+          mode = "nts";
+          address = "time.cloudflare.com";
+        }
+        {
+          mode = "nts";
+          address = "oregon.time.system76.com";
+        }
+        {
+          mode = "pool";
+          address = "time.nist.gov";
+        }
       ];
     };
   };
