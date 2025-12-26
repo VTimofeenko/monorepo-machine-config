6741e0c61ff6007894665176b96c550baa8ee489
Vladimir Timofeenko <id@vtimofeenko.com>
2025-12-25 17:48:19 -0800

    refactor(homelab): change kea control socket syntax to the new version

--- nixosModules/services/dhcp/non-functional/metrics.nix
+++ nixosModules/services/dhcp/non-functional/metrics.nix
@@ -11,22 +11,15 @@
       true;
     listenAddress = lib.homelab.getOwnIpInNetwork "backbone-inner";
     openFirewall = lib.mkForce false;
-    targets = [
-      config.services.kea.dhcp4.settings.control-socket.socket-name
-    ];
-    # [25.11]
-    # This is for future when it's a list
-    # lib.catAttrs "socket-name" config.services.kea.dhcp4.settings.control-sockets;
+    targets = lib.catAttrs "socket-name" config.services.kea.dhcp4.settings.control-sockets;
   };
 
-  # [25.11]
-  # Explodes in 2.7 – this will be a list.
-  # `kea` and the exporter run as the same user. Unix security is fine for
-  # now
-  services.kea.dhcp4.settings.control-socket = {
-    socket-type = "unix";
-    socket-name = "/run/kea/control.sock";
-  };
+  services.kea.dhcp4.settings.control-sockets = [
+    {
+      socket-type = "unix";
+      socket-name = "/run/kea/control.sock";
+    }
+  ];
 
   imports = [
     (self.serviceModules.prometheus.srvLib.mkBackboneInnerFirewallRules {
@@ -34,5 +27,4 @@
       ports = config.services.prometheus.exporters.kea.port;
     })
   ];
-
 }
