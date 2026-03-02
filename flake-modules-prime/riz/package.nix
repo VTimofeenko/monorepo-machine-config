58c3b3212a2a52ff2073d3e1f731e7a987ab0779
Vladimir Timofeenko <id@vtimofeenko.com>
2026-03-02 13:19:58 -0800

    chore: add licenses and bump riz package

--- flake-modules-prime/riz/package.nix
+++ flake-modules-prime/riz/package.nix
@@ -1,26 +1,29 @@
 {
   fetchFromGitHub,
   rustPlatform,
+  lib,
 }:
 
 rustPlatform.buildRustPackage rec {
   pname = "riz";
-  version = "v0.2.0-git";
+  version = "0.2.0";
 
   src = fetchFromGitHub {
     owner = "a-tal";
     repo = pname;
-    rev = "99383bf1b4ea8e33ed1432ec8d9b3e5544eda279";
+    rev = "7247539d8db1941cda5ab4c67e98f5faa63c4dbd";
     hash = "sha256-1iH+lJrYc3AR/9tfkVn22bUgqbmffhBR0n2ou65xoCs=";
   };
 
-  cargoHash = "sha256-VagvZcwpAMaL5C/fgmA28EpDb1eXzWGJNP4KMYJlkrQ=";
+  cargoHash = "sha256-Gl/m0zB/IqP0votmEkEAONAZuYwAB3i+wBrB9vCgfvw=";
 
   meta = {
     description = "Wiz lights API and CLI ";
     homepage = "https://github.com/a-tal/riz";
-    # No license :(
-    # license = licenses.unlicense;
+    license = with lib.licenses; [
+      mit
+      asl20
+    ];
     # maintainers = [];
   };
 }
