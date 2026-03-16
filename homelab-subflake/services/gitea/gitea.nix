2a43b155fa39187bf198164a6db7c16246068934
Vladimir Timofeenko <id@vtimofeenko.com>
2026-03-16 10:49:39 -0700

    refactor(gitea): move capabilities into the main module

--- homelab-subflake/services/gitea/gitea.nix
+++ homelab-subflake/services/gitea/gitea.nix
@@ -25,6 +25,15 @@ in
       };
     };
   };
+
+  # Allow binding on port 22 (privileged port)
+  # These settings enable Gitea built-in SSH server to bind to port 22
+  systemd.services.gitea.serviceConfig = {
+    AmbientCapabilities = lib.mkOverride 10 "CAP_NET_BIND_SERVICE";
+    CapabilityBoundingSet = lib.mkOverride 10 "CAP_NET_BIND_SERVICE";
+    # TODO: check why/if this is needed. Was it for mail?
+    PrivateUsers = lib.mkOverride 10 false;
+  };
   imports = [
     ./non-functional/dumps.nix
     ./functional/mailer.nix
--- homelab-subflake/services/gitea/non-functional/endpoints-config.nix
+++ homelab-subflake/services/gitea/non-functional/endpoints-config.nix
@@ -8,13 +8,4 @@ in
     SSH_PORT = endpoints.ssh.port;
     SSH_LISTEN_HOST = lib.homelab.getOwnIpInNetwork listenInterface;
   };
-
-  # Allow binding on port 22
-  systemd.services.gitea.serviceConfig = {
-    # These settings enable Gitea built-in server to bind to port 22
-    # Source: archwiki
-    AmbientCapabilities = "CAP_NET_BIND_SERVICE";
-    CapabilityBoundingSet = lib.mkForce "CAP_NET_BIND_SERVICE";
-    PrivateUsers = lib.mkForce false;
-  };
 }
