a56e6603b6c340cf0d9fad4cfa97cb4161805090
Vladimir Timofeenko <id@vtimofeenko.com>
2026-03-20 23:27:27 -0700

    chore: fmt

--- homelab-subflake/services/auth_dns/srv-lib.nix
+++ homelab-subflake/services/auth_dns/srv-lib.nix
@@ -1,7 +1,8 @@
 { lib, ... }:
 let
   # DNS record formatters
-  mkRecord = recordType: domainName: recordValue:
+  mkRecord =
+    recordType: domainName: recordValue:
     "${domainName} IN ${recordType} ${recordValue}";
   mkARecord = mkRecord "A";
   mkCNAMERecord = domainName: recordValue: (mkRecord "CNAME" domainName (recordValue + "."));
@@ -17,12 +18,13 @@ rec {
     auth_dns (NSD) is localhost-only and not directly accessible to clients.
     Returns list of IPs (e.g., ["192.168.1.1", "192.168.1.2"])
   */
-  getNameserverIPs = { lib }:
+  getNameserverIPs =
+    { lib }:
     lib.homelab.services.getAll
     |> lib.filterAttrs (_name: svcData: (svcData.moduleName or _name) == "dns")
     |> lib.attrNames
     |> map lib.homelab.getServiceInnerIP
-    |> lib.take 2;  # Typically `ns1` and `ns2`
+    |> lib.take 2; # Typically `ns1` and `ns2`
 
   /**
     Create base zone configuration with SOA and NS records.
@@ -34,36 +36,49 @@ rec {
 
     Returns NSD zone config with SOA/NS headers
   */
-  mkZoneBase = { domain, nameserverIPs, lib, ttl ? 1800 }: {
-    data = ''
-      $ORIGIN ${domain}.
-      $TTL ${toString ttl}
+  mkZoneBase =
+    {
+      domain,
+      nameserverIPs,
+      lib,
+      ttl ? 1800,
+    }:
+    {
+      data = ''
+        $ORIGIN ${domain}.
+        $TTL ${toString ttl}
 
-      @ IN SOA ns1.${domain}. admin.${domain}. (
-          ${builtins.readFile ./serial} ; serial number
-          28800 ; Refresh
-          7200 ; Retry
-          864000 ; Expire
-          86400 ; Min TTL
-          )
+        @ IN SOA ns1.${domain}. admin.${domain}. (
+            ${builtins.readFile ./serial} ; serial number
+            28800 ; Refresh
+            7200 ; Retry
+            864000 ; Expire
+            86400 ; Min TTL
+            )
 
-      ${lib.concatLines (lib.imap1 (i: _ip: "IN NS ns${toString i}.${domain}.") nameserverIPs)}
+        ${lib.concatLines (lib.imap1 (i: _ip: "IN NS ns${toString i}.${domain}.") nameserverIPs)}
 
-      ${lib.concatLines (lib.imap1 (i: ip: "ns${toString i} IN A ${ip}") nameserverIPs)}
+        ${lib.concatLines (lib.imap1 (i: ip: "ns${toString i} IN A ${ip}") nameserverIPs)}
 
-    '';
-  };
+      '';
+    };
 
-  /** Returns a list of zones that `auth_dns` manages */
+  /**
+    Returns a list of zones that `auth_dns` manages
+  */
   getZones =
     let
       srvDomain = lib.homelab.getSettings.publicDomainName;
     in
     [
-    srvDomain
-    "metrics.${srvDomain}"
-  ]
-  ++
-  # Assemble domains from networks
-  (lib.homelab.networks.getAll |> builtins.mapAttrs(_: builtins.getAttr "domain")  |> builtins.attrValues) ;
+      srvDomain
+      "metrics.${srvDomain}"
+    ]
+    ++
+      # Assemble domains from networks
+      (
+        lib.homelab.networks.getAll
+        |> builtins.mapAttrs (_: builtins.getAttr "domain")
+        |> builtins.attrValues
+      );
 }
