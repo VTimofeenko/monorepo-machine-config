873f86f3cefc8073f8c8b9314583d043f9615ffe
Vladimir Timofeenko <id@vtimofeenko.com>
2026-03-27 18:59:53 -0700

    fix(vim): force exit nls when closing nickel

--- flake-modules-prime/neovim/config/max/z-lang-nickel.nix
+++ flake-modules-prime/neovim/config/max/z-lang-nickel.nix
@@ -7,10 +7,13 @@
   pkgs-unstable,
   ...
 }:
+let
+  settings.nlsPkg = pkgs-unstable.nls;
+in
 {
   plugins = [
     /**
-      vim-nickel provides syntax highlighting, file detection and identation for Nickel source files.
+      `vim-nickel` provides syntax highlighting, file detection and indentation for Nickel source files.
       It's needed to actually kick off the LSP
     */
     pkgs.vimPlugins.vim-nickel
@@ -19,12 +22,29 @@
   config = [
     # LSP configuration
     ''
-    vim.lsp.config.nickel_ls = {
-      cmd = { '${lib.getExe pkgs-unstable.nls}' },
-      autostart = true,
-    }
-    vim.lsp.enable('nickel_ls')
+      vim.lsp.config.nickel_ls = {
+        cmd = { '${lib.getExe settings.nlsPkg}' },
+        autostart = true,
+      }
+      vim.lsp.enable('nickel_ls')
     ''
+
+    # Force stop nickel_ls on buffer unload to avoid slow shutdown
+    # Revisit later, this assert will take care of that
+    (
+      assert lib.assertMsg (lib.versionOlder settings.nlsPkg.version "1.16.0") "Check force exit nls hook in vim, may be fixed";
+      ''
+        vim.api.nvim_create_autocmd("BufUnload", {
+          pattern = "*.ncl",
+          callback = function()
+          for _, client in ipairs(vim.lsp.get_clients({ name = "nickel_ls" })) do
+          vim.lsp.stop_client(client.id, true)
+          end
+          end,
+        })
+      ''
+    )
+
     # nickel templates for luasnip
     ''
       ls.add_snippets("nickel", {
