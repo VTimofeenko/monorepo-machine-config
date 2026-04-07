6c794c5c6dc76b7eb39aff1155397ef35918317e
Vladimir Timofeenko <id@vtimofeenko.com>
2026-04-07 12:05:23 -0700

    fix(prometheus): integrate with alertmanager properly

--- homelab-subflake/services/prometheus/service.nix
+++ homelab-subflake/services/prometheus/service.nix
@@ -3,6 +3,13 @@
   services.prometheus = {
     enable = true;
     retentionTime = "120d";
+    alertmanagers = [
+      {
+        static_configs = [
+          { targets = [ "${lib.homelab.getServiceInnerIP "alert-manager"}:9093" ]; }
+        ];
+      }
+    ];
   };
 
   imports = [
