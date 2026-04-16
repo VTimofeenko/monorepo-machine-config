729bc8efbce8b47572f290fed0d6f7ee121bc69c
Vladimir Timofeenko <id@vtimofeenko.com>
2026-04-15 21:23:48 -0700

    refactor: migrate boron into homelab subflake

---
+++ homelab-subflake/hosts/boron/configuration/default.nix
@@ -0,0 +1,75 @@
+{
+  pkgs,
+  lib,
+  nixos-hardware,
+  ...
+}:
+{
+  # Boot
+  boot = {
+    initrd = {
+      availableKernelModules = [
+        "xhci_pci"
+        "usbhid"
+        "usb_storage"
+        "hid_logitech_hidpp"
+        "xhci_pci_renesas"
+      ];
+      kernelModules = [
+        "xhci_pci"
+        "usbhid"
+        "usb_storage"
+        "hid_logitech_hidpp"
+        "xhci_pci_renesas"
+      ];
+    };
+    supportedFilesystems = lib.mkForce [
+      "btrfs"
+      "reiserfs"
+      "vfat"
+      "f2fs"
+      "xfs"
+      "ntfs"
+      "cifs"
+      "ext4"
+    ];
+    kernelPackages = pkgs.linuxPackages_latest;
+    tmp = {
+      useTmpfs = true;
+      tmpfsSize = "1G";
+    };
+    kernelParams = [
+      "8250.nr_uarts=1"
+      "console=ttyAMA0,115200"
+      "console=tty1"
+      "boot.shell_on_fail"
+      # ```
+      # ID 174c:1153 ASMedia Technology Inc. ASM1153 SATA 3Gb/s bridge
+      # ```
+      # Is prone to I/O errors if using `uas` driver. This
+      # forces the `usb-storage` one. See lithium config for similar example.
+      "usb-storage.quirks=174c:1153:u"
+    ];
+    consoleLogLevel = 7;
+    loader = {
+      grub.enable = false;
+      generic-extlinux-compatible.enable = true;
+    };
+  };
+
+  # File systems
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/NIXOS_SD";
+    fsType = "ext4";
+    options = [ "noatime" ];
+  };
+
+  # Misc
+  hardware.enableRedistributableFirmware = true;
+  system.stateVersion = "22.05";
+
+  # Imports
+  imports = [
+    nixos-hardware.nixosModules.raspberry-pi-4
+  ];
+}
