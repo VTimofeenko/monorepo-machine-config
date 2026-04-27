722a52be51cb358067835a4799bf03141ec44391
Vladimir Timofeenko <id@vtimofeenko.com>
2026-04-27 11:27:54 -0700

    fix(homebox): properly disable github release check

--- homelab-subflake/services/homebox/homebox.nix
+++ homelab-subflake/services/homebox/homebox.nix
@@ -2,7 +2,7 @@
   services.homebox.enable = true;
 
   services.homebox.settings = {
-    HBOX_OPTIONS_CHECK_GITHUB_RELEASE = "false";
+    HBOX_OPTIONS_GITHUB_RELEASE_CHECK = "false";
   };
 
   imports = [
