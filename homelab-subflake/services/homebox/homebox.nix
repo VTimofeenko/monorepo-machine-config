688e948369df5f694b7feeb7cf25590431bfdb0e
Vladimir Timofeenko <id@vtimofeenko.com>
2026-04-27 15:19:30 -0700

    fix(homebox): fix the attachment rendering infinite loop

--- homelab-subflake/services/homebox/homebox.nix
+++ homelab-subflake/services/homebox/homebox.nix
@@ -1,5 +1,14 @@
+{ pkgs-unstable, lib, ... }:
+let
+  homeBoxPkg = pkgs-unstable.homebox;
+in
 {
   services.homebox.enable = true;
+
+  services.homebox.package =
+    assert lib.assertMsg (lib.versionOlder homeBoxPkg.version "1.25.0")
+      "Check if the override is still necessary";
+    homeBoxPkg;
 
   services.homebox.settings = {
     HBOX_OPTIONS_GITHUB_RELEASE_CHECK = "false";
