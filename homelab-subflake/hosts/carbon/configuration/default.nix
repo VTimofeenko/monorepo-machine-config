69da7a990f40dce98c72de4c85eeb1c695b61f71
Vladimir Timofeenko <id@vtimofeenko.com>
2026-04-30 08:40:11 -0700

    refactor(carbon): migrate into homelab subflake

---
+++ homelab-subflake/hosts/carbon/configuration/default.nix
@@ -0,0 +1,49 @@
+{ lib, ... }:
+{
+  # Boot
+  boot = {
+    # Sets screen timeout to 1 minute
+    kernelParams = [ "consoleblank=60" ];
+    initrd = {
+      availableKernelModules = [
+        "xhci_pci"
+        "ehci_pci"
+        "ahci"
+        "usb_storage"
+        "sd_mod"
+        "rtsx_pci_sdmmc"
+      ];
+      kernelModules = [ ];
+    };
+    kernelModules = [ "kvm-intel" ];
+    extraModulePackages = [ ];
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+  };
+
+  # File systems
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/nixos";
+      fsType = "ext4";
+      options = [ "noatime" ];
+    };
+    "/boot" = {
+      device = "/dev/disk/by-label/boot";
+      fsType = "vfat";
+    };
+  };
+  swapDevices = [ { device = "/dev/disk/by-label/swap"; } ];
+
+  # Misc
+  system.stateVersion = "22.11";
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware = {
+    cpu.intel.updateMicrocode = lib.mkForce true;
+    enableRedistributableFirmware = true;
+  };
+  # Imports
+  imports = [ ];
+}
