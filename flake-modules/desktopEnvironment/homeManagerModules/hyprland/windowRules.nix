fc2c69bd2dad3e8d0681f8f3c2a32270d44eb717
Vladimir Timofeenko <id@vtimofeenko.com>
2026-07-01 15:51:32 -0700

    fix(hyprland): migrate to the new windowrule syntax

--- flake-modules/desktopEnvironment/homeManagerModules/hyprland/windowRules.nix
+++ flake-modules/desktopEnvironment/homeManagerModules/hyprland/windowRules.nix
@@ -11,6 +11,7 @@ let
     titles = [
       "Open File"
       ".*wants to save" # Odd brave's behavior
+      ".*wants to open" # Odd brave's behavior
     ];
     tagName = "flyout";
   };
@@ -19,21 +20,14 @@ in
 {
   wayland.windowManager.hyprland.settings = {
     # "Flyout" is effectively a modal that should be created in the center top of the screen.
-    windowrulev2 =
+    windowrule =
       [
         # Assign the tags
-        (settings.flyOut.classes |> map (class: "tag +${settings.flyOut.tagName}, class:(${class})"))
-        (settings.flyOut.titles |> map (title: "tag +${settings.flyOut.tagName}, title:(${title})"))
+        (settings.flyOut.classes |> map (class: "tag +${settings.flyOut.tagName}, match:class (${class})"))
+        (settings.flyOut.titles |> map (title: "tag +${settings.flyOut.tagName}, match:title (${title})"))
         # OpenSnitch, but only the prompt
-        "tag +${settings.flyOut.tagName}, class:(opensnitch_ui), title:(OpenSnitch v.*)"
-        # Implement the tag window rules
-        (map (rule: "${rule}, tag:${settings.flyOut.tagName}") [
-          "float"
-          "size 30% 20%"
-          "move 35% 10%"
-          "stayfocused"
-          "dimaround"
-        ])
+        "tag +${settings.flyOut.tagName}, match:class (opensnitch_ui), match:title (OpenSnitch v.*)"
+        "float on, size (monitor_w*0.3) (monitor_h*0.2), move ((monitor_w*0.35)) ((monitor_h*0.1)), stay_focused on, dim_around on, match:tag ${settings.flyOut.tagName}"
       ]
       |> lib.flatten;
   };
